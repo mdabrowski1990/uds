0de4e7a8aa00368753364d9e427685c42bd2cc05
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-03-02 16:19:02 +0100

    server_scheme
    Scheme of Server class.
    Introduction of constants - shared module.

---
+++ uds/constants/timing.py
@@ -0,0 +1,6 @@
+"""Storage for timing (performance) constants defined by UDS ISO standards."""
+
+from .typing import TypingTimeMilliseconds
+
+DEFAULT_P2_SERVER_MAX: TypingTimeMilliseconds = 50
+DEFAULT_P2EXT_SERVER_MAX: TypingTimeMilliseconds = 5000
---
+++ uds/constants/typing.py
@@ -0,0 +1,7 @@
+"""Storage for constants used for typing (mainly aliases)."""
+
+__all__ = ["TypingTimeMilliseconds"]
+
+from typing import Union
+
+TypingTimeMilliseconds = Union[int, float]
