c7f7fdf8d06428458fd093be54181997a5be4784
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-03-07 11:22:38 +0100

    devide_response_rule
    Separate 'ResponseRules' from response_manager.

---
+++ tests/uds/server/test_response_rule.py
@@ -0,0 +1,95 @@
+import pytest
+from mock import Mock
+
+from uds.server.response_rule import ResponseRule, AddressingType
+
+
+class TestResponseRule:
+
+    def setup(self):
+        self.mock_response_rule = Mock(spec=ResponseRule)
+
+    # __init__
+
+    @pytest.mark.parametrize("addressing_types", [
+        tuple(AddressingType),
+        [AddressingType.PHYSICAL],
+        {AddressingType.BROADCAST, AddressingType.FUNCTIONAL}
+    ])
+    @pytest.mark.parametrize("related_request_sids", [
+        (0x10, 0x11),
+        [0x01, 0x02, 0x03],
+        {0xFF}
+    ])
+    def test_init(self, addressing_types, related_request_sids):
+        ResponseRule.__init__(self=self.mock_response_rule, addressing_types=addressing_types,
+                              related_request_sids=related_request_sids)
+        # data validation
+        self.mock_response_rule._ResponseRule__validate_addressing_types.assert_called_once_with(
+            addressing_types=addressing_types)
+        self.mock_response_rule._ResponseRule__validate_related_request_sids.assert_called_once_with(
+            related_request_sids=related_request_sids)
+        # verify attributes values
+        assert ResponseRule.addressing_types.fget(self.mock_response_rule) == set(addressing_types)
+        assert ResponseRule.related_request_sids.fget(self.mock_response_rule) == set(related_request_sids)
+
+    # __validate_addressing_types
+
+    @pytest.mark.parametrize("addressing_types", [
+        tuple(AddressingType),
+        [AddressingType.PHYSICAL],
+        {AddressingType.BROADCAST, AddressingType.FUNCTIONAL}
+    ])
+    def test_validate_addressing_types__valid(self, addressing_types):
+        assert ResponseRule._ResponseRule__validate_addressing_types(addressing_types=addressing_types) is None
+
+    @pytest.mark.parametrize("addressing_types", [None, 1, 6.5, False])
+    def test_validate_addressing_types__invalid_type(self, addressing_types):
+        with pytest.raises(TypeError):
+            ResponseRule._ResponseRule__validate_addressing_types(addressing_types=addressing_types)
+
+    @pytest.mark.parametrize("addressing_types", [
+        [None],
+        {1, 2, 3, 4},
+        ("xyz", "abcd"),
+    ])
+    def test_validate_addressing_types__invalid_value(self, addressing_types):
+        with pytest.raises(ValueError):
+            ResponseRule._ResponseRule__validate_addressing_types(addressing_types=addressing_types)
+
+    # __validate_related_request_sids
+
+    @pytest.mark.parametrize("related_request_sids", [
+        (0x10, 0x11),
+        [0x01, 0x02, 0x03],
+        {0xFF}
+    ])
+    def test_validate_related_request_sids__valid(self, related_request_sids):
+        assert ResponseRule._ResponseRule__validate_related_request_sids(
+            related_request_sids=related_request_sids) is None
+
+    @pytest.mark.parametrize("related_request_sids", [None, 1, 6.5, False])
+    def test_validate_related_request_sids__invalid_type(self, related_request_sids):
+        with pytest.raises(TypeError):
+            ResponseRule._ResponseRule__validate_related_request_sids(related_request_sids=related_request_sids)
+
+    @pytest.mark.parametrize("related_request_sids", [
+        [None],
+        {1.5, 2, 3, 4},
+        ("xyz", "abcd")
+    ])
+    def test_validate_related_request_sids__invalid_value(self, related_request_sids):
+        with pytest.raises(ValueError):
+            ResponseRule._ResponseRule__validate_related_request_sids(related_request_sids=related_request_sids)
+
+    # addressing_types
+
+    def test_addressing_types__cannot_change(self, example_addressing_types):
+        with pytest.raises(Exception):
+            ResponseRule.addressing_types.fset(self.mock_response_rule, example_addressing_types)
+
+    # related_request_sids
+
+    def test_related_request_sids__cannot_change(self, example_request_sids):
+        with pytest.raises(Exception):
+            ResponseRule.related_request_sids.fset(self.mock_response_rule, example_request_sids)
--- uds/server/response_manager.py
+++ uds/server/response_manager.py
@@ -6,93 +6,11 @@ ResponseManager is meant to:
  - contain ordered rules which determines how to create a response message to any received request
 """
 
-__all__ = ["ResponseRule", "ResponseManager"]
-
-from abc import ABC, abstractmethod
-from typing import Optional
+__all__ = ["ResponseManager"]
 
 from .types import *
 from .server_state import ServerState
-
-
-
-
-class ResponseRule(ABC):
-    """A single rule for creating response message by a server."""
-
-    def __init__(self, addressing_types: AddressingTypes, related_request_sids: RequestSIDs) -> None:
-        """
-        Configure the rule.
-
-        :param addressing_types: Addressing Type for which the rule is applicable.
-        :param related_request_sids: Service Identifiers for which the rule is applicable.
-        """
-        self.__validate_addressing_types(addressing_types=addressing_types)
-        self.__validate_related_request_sids(related_request_sids=related_request_sids)
-        self.__addressing_types = set(addressing_types)
-        self.__related_request_sids = set(related_request_sids)
-
-    @staticmethod
-    def __validate_addressing_types(addressing_types: AddressingTypes) -> None:
-        """
-        Verify addressing types argument.
-
-        :param addressing_types: Addressing types to validate.
-
-        :raise TypeError: Addressing types argument is not Iterable.
-        :raise ValueError: At least one member of addressing types is not AddressingType.
-        """
-        if not isinstance(addressing_types, (set, tuple, list)):
-            raise TypeError("'addressing_types' is not iterable")
-        if not all([isinstance(addressing_type, AddressingType) for addressing_type in addressing_types]):
-            raise ValueError("'addressing_types' does not contain instances of AddressingType only")
-
-    @staticmethod
-    def __validate_related_request_sids(related_request_sids: RequestSIDs) -> None:
-        """
-        Verify related requests SIDs argument.
-
-        :param related_request_sids: Requests SIDs to validate.
-
-        :raise TypeError: Related requests SIDs argument is not Iterable.
-        :raise ValueError: At least one member of related requests sids is not raw byte.
-        """
-        if not isinstance(related_request_sids, (set, tuple, list)):
-            raise TypeError("'related_request_sids' is not iterable")
-        if not all([isinstance(sid, int) and 0x00 <= sid <= 0xFF for sid in related_request_sids]):
-            raise ValueError("'related_request_sids' does not contain raw bytes only")
-
-    @abstractmethod
-    def is_triggered(self, request: UdsRequest, current_states: CurrentStatesValues) -> bool:
-        """
-        Check if the rule might be used to generate a response message for the received request.
-
-        :param request: Request message that was received.
-        :param current_states: Current values of all server's states.
-
-        :return: True if the rule is applicable in provided situation, False otherwise.
-        """
-
-    @abstractmethod
-    def create_response(self, request: UdsRequest, current_states: CurrentStatesValues) -> Optional[UdsResponse]:
-        """
-        Create response message according to the rule.
-
-        :param request: Request message for which response to be generated.
-        :param current_states: Current values of all server's states.
-
-        :return: Response message that was generated. None if no message to be sent in the response.
-        """
-
-    @property
-    def addressing_types(self) -> AddressingTypes:
-        """Addressing types of incoming requests for which this rule is supported."""
-        return self.__addressing_types
-
-    @property
-    def related_request_sids(self) -> RequestSIDs:
-        """Service Identifiers of incoming requests for which this rule is supported."""
-        return self.__related_request_sids
+from .response_rule import ResponseRule
 
 
 Rules = List[ResponseRule]
