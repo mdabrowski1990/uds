396423d6934ae5132de62a2f09309613d6f0825e
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-03-07 10:01:10 +0100

    todo_updated
    Provide better information for human being in TODOs

--- uds/messages/types.py
+++ uds/messages/types.py
@@ -6,9 +6,11 @@ from typing import Sequence
 
 from .pdu import AbstractPDU
 
-# TODO: since python 3.9 it can be replaced with Union[list[int], tuple[int]]; keep this way for backward compatibility
-# TODO: prospector supports only pylint version 2.5 that has serious problem with Aliases in Python 3.9.
-#  Remove unsubscriptable-object once prospector supports newer versions of pylint.
 RawByte = int
+# TODO: since python 3.9 Sequence can be replaced with Union[list[int], tuple[int]]
+#  keep Sequences way for backward compatibility with older Python version or create specific file version for
+#  Python 3.9 or newer
+# TODO: prospector supports only pylint version 2.5 that has serious problem with Aliases in Python 3.9.
+#  Remove unsubscriptable-object once prospector supports newer versions of pylint for Python 3.9.
 RawMessage = Sequence[RawByte]  # pylint: disable=unsubscriptable-object
 PDUs = Sequence[AbstractPDU]  # pylint: disable=unsubscriptable-object
