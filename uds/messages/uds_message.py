67819b442adfa29cf97101f588b1930986a5add2
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-08-18 13:58:17 +0200

    complete_refinement
    Full refinement of UDS Messages implementation.
    To be review etc.

--- uds/messages/uds_message.py
+++ uds/messages/uds_message.py
@@ -1,148 +1,165 @@
 """Common implementation of all UDS messages (requests and responses)."""
 
-__all__ = ["UdsMessage", "UdsResponseType"]
+__all__ = ["UdsMessage", "UdsMessageRecord"]
 
-from typing import Optional, Union, Tuple, List
+from typing import Union, Tuple, List
 
-from uds.utilities import RawBytes, RawBytesTuple, ValidatedEnum
-from .transmission_attributes import AddressingType
-from .pdu import AbstractNPDU
+from uds.utilities import RawBytes, RawBytesTuple, validate_raw_bytes, ReassignmentError, TimeStamp
+from .transmission_attributes import AddressingType, AddressingMemberTyping, TransmissionDirection
+from .npdu import AbstractNPDURecord
 
-PDUsTuple = Tuple[AbstractNPDU, ...]
-PDUs = Union[PDUsTuple, List[AbstractNPDU]]  # pylint: disable=unsubscriptable-object
-
-
-class UdsResponseType(ValidatedEnum):
-    """
-    Types of UDS response messages.
-
-    Options:
-    - POSITIVE - response messages
-        !WARNING! Note that this status does not reflect compatibility with message format (messaging database).
-    - NEGATIVE - negative response messages in format [0x7F, SID, NRC]
-        Where:
-        SID - Service Identifier of request (identifies request to which it is response)
-        NRC - Negative Response Code (provides reason why responded negatively)
-    - INVALID - response messages that is incompatible with UDS standard
-    """
-
-    POSITIVE = "Positive Response Message"  # noqa: F841
-    NEGATIVE = "Negative Response Message"  # noqa: F841
-    INVALID = "Invalid"  # noqa: F841
+NPDURecordsTuple = Tuple[AbstractNPDURecord, ...]
+NPDURecordsSequence = Union[NPDURecordsTuple, List[AbstractNPDURecord]]  # pylint: disable=unsubscriptable-object
 
 
 class UdsMessage:
-    """Common implementation of all UDS messages (requests and responses)."""
+    """Definition a diagnostic messages that are exchanged by UDS servers and clients."""
 
-    def __init__(self,
-                 raw_message: RawBytes,
-                 addressing: Optional[AddressingType] = None,  # pylint: disable=unsubscriptable-object
-                 pdu_sequence: Optional[PDUs] = None) -> None:  # pylint: disable=unsubscriptable-object
+    def __init__(self, raw_message: RawBytes, addressing: AddressingMemberTyping) -> None:
         """
-        Create storage for a single UDS message.
+        Create a storage for information of a single diagnostic message.
 
-        :param raw_message: A single message that consists of raw bytes only.
+        :param raw_message: Raw bytes of data that this diagnostic message carries.
         :param addressing: Addressing type for which this message is relevant.
-            This parameter does not have to be set if 'pdu_sequence' is provided.
-        :param pdu_sequence: Sequence of PDUs (Protocol Data Units) that were published to a bus to send/receive
-            this messages. It should be None, if message was never transmitted.
         """
-        # verify arguments
-        self.__validate_raw_message(raw_message=raw_message)
-        if pdu_sequence is not None:
-            self.__validate_pdu_sequence(pdu_sequence=pdu_sequence)
-        if addressing is not None:
-            self.__validate_addressing(addressing=addressing)
-        # update data
-        self.__raw_message = tuple(raw_message)
-        self.__pdu_sequence = tuple() if pdu_sequence is None else tuple(pdu_sequence)
-        self.__addressing = addressing
-
-    @staticmethod
-    def __validate_raw_message(raw_message: RawBytes) -> None:
-        """
-        Verify raw message argument.
-
-        :param raw_message: Raw message value to validate.
-
-        :raise TypeError: Raw message is not list or tuple type.
-        :raise ValueError: At least one member of raw message elements is not raw byte value.
-        """
-        if not isinstance(raw_message, (tuple, list)):
-            raise TypeError("'raw_message' is not list or tuple type")
-        if not all([isinstance(raw_byte, int) and 0x00 <= raw_byte <= 0xFF for raw_byte in raw_message]):
-            raise ValueError("'raw_message' does not contain raw bytes (int value between 0 and 255) values only")
-
-    @staticmethod
-    def __validate_pdu_sequence(pdu_sequence: PDUs) -> None:
-        """
-        Verify pdu sequence argument.
-
-        :param pdu_sequence: PDUs sequence to validate.
-
-        :raise TypeError: PDUs sequence is not list or tuple type.
-        :raise ValueError: At least one of PDUs sequence elements is not PDU.
-        """
-        if not isinstance(pdu_sequence, (tuple, list)):
-            raise TypeError("'pdu_sequence' is not list or tuple type")
-        if not all([isinstance(pdu, AbstractNPDU) for pdu in pdu_sequence]):
-            raise ValueError("'pdu_sequence' does not contain AbstractPDU instances only")
-
-    @staticmethod
-    def __validate_addressing(addressing: AddressingType) -> None:
-        """
-        Verify addressing type argument.
-
-        :param addressing: Type of addressing to validate.
-
-        :raise TypeError: Addressing argument is not instance of AddressingType.
-        """
-        if not isinstance(addressing, AddressingType):
-            raise TypeError("'addressing' is instance of AddressingType")
-
-    @property
-    def pdu_sequence(self) -> PDUsTuple:
-        """
-        Get PDUs that were sent/received to transmit this message over any bus.
-
-        :return: PDUs list that were sent/received or empty list if this message was never transmitted.
-        """
-        return self.__pdu_sequence
+        self.raw_message = raw_message  # type: ignore
+        self.addressing = addressing  # type: ignore
 
     @property
     def raw_message(self) -> RawBytesTuple:
-        """Raw message that this message carries."""
+        """Raw bytes of data that this diagnostic message carries."""
         return self.__raw_message
 
-    @property
-    def addressing(self) -> Optional[AddressingType]:  # pylint: disable=unsubscriptable-object
+    @raw_message.setter
+    def raw_message(self, value: RawBytes):
         """
-        Addressing type over which this message was transmitted.
+        Set value of raw message which this diagnostic message carries.
 
-        :return: Addressing over which this message was sent/received. None if messages was never transmitted.
+        :param value: Raw message value to set.
         """
-        if self.pdu_sequence:
-            return self.pdu_sequence[0].addressing
+        validate_raw_bytes(value)
+        self.__raw_message = tuple(value)
+
+    @property
+    def addressing(self) -> AddressingType:
+        """Addressing type for which this message is relevant."""
         return self.__addressing
 
-    # @property  # noqa: F841
-    # def time_transmission_start(self) -> Optional[datetime]:  # pylint: disable=unsubscriptable-object
-    #     """
-    #     Time when the message transmission to a bus was started.
-    #
-    #     It is determined by a time when the first PDU (that carries this message) was either received or transmitted.
-    #
-    #     :return: Date and time when the message transmission was initiated. None if message was not transmitted.
-    #     """
-    #     return self.pdu_sequence[0].time_transmitted if self.pdu_sequence else None
-    #
-    # @property
-    # def time_transmission_end(self) -> Optional[datetime]:  # pylint: disable=unsubscriptable-object
-    #     """
-    #     Time when the message transmission to a bus was finished.
-    #
-    #     It is determined by a time when the last PDU (that carries this message) was either received or transmitted.
-    #
-    #     :return: Date and time when the message transmission was completed. None if message was not transmitted.
-    #     """
-    #     return self.pdu_sequence[-1].time_transmitted if self.pdu_sequence else None
+    @addressing.setter
+    def addressing(self, value: AddressingMemberTyping):
+        """
+        Set value of addressing type for which this diagnostic message is relevant.
+
+        :param value: Addressing value to set.
+        """
+        AddressingType.validate_member(value)
+        self.__addressing = AddressingType(value)
+
+
+class UdsMessageRecord:
+    """Definition of a record that stores historic information about transmitted or received UDSMessage."""
+
+    def __init__(self, raw_message: RawBytes, npdu_sequence: NPDURecordsSequence) -> None:
+        """
+        Create historic record of a diagnostic message that was either received of transmitted to a bus.
+
+        :param raw_message: Raw bytes of data that this diagnostic message carried.
+        :param npdu_sequence: Record of N_PDUs (in transmission order) that carried this diagnostic message.
+        """
+        self.raw_message = raw_message  # type: ignore
+        self.npdu_sequence = npdu_sequence  # type: ignore
+
+    @staticmethod
+    def __validate_npdu_sequence(npdu_sequence: NPDURecordsSequence) -> None:
+        """
+        Validate N_PDUs sequence argument.
+
+        :param npdu_sequence: Value of N_PDUs sequence to validate.
+
+        :raise TypeError: N_PDUs sequence is not list or tuple type.
+        :raise ValueError: At least one of N_PDUs sequence elements is not an object of N_PDU.
+        """
+        if not isinstance(npdu_sequence, (tuple, list)):
+            raise TypeError(f"Provided value of 'npdu_sequence' is not list or tuple type. "
+                            f"Actual type: {type(npdu_sequence)}.")
+        if not npdu_sequence or any([not isinstance(npdu, AbstractNPDURecord) for npdu in npdu_sequence]):
+            raise ValueError(f"Provided value of 'npdu_sequence' must contain only instances of AbstractNPDURecord "
+                             f"class. Actual value: {npdu_sequence}")
+
+    @property
+    def raw_message(self) -> RawBytesTuple:
+        """Raw bytes of data that this diagnostic message carried."""
+        return self.__raw_message
+
+    @raw_message.setter
+    def raw_message(self, value: RawBytes):
+        """
+        Set value of raw message which this diagnostic message carried.
+
+        :param value: Raw message value to set.
+
+        :raise ReassignmentError: There is a call to change the value after the initial assignment (in __init__).
+        """
+        try:
+            self.__getattribute__("_UdsMessageRecord__raw_message")
+        except AttributeError:
+            validate_raw_bytes(value)
+            self.__raw_message = tuple(value)
+        else:
+            raise ReassignmentError("You cannot change value of 'raw_message' attribute once it is assigned.")
+
+    @property
+    def npdu_sequence(self) -> NPDURecordsTuple:
+        """Sequence (in transmission order) of Network Protocol Data Units that carried this diagnostic message."""
+        return self.__npdu_sequence
+
+    @npdu_sequence.setter
+    def npdu_sequence(self, value: NPDURecordsSequence):
+        """
+        Assign N_PDUs which carried this diagnostic message .
+
+        :param value: N_PDUs sequence value to set.
+
+        :raise ReassignmentError: There is a call to change the value after the initial assignment (in __init__).
+        """
+        try:
+            self.__getattribute__("_UdsMessageRecord__npdu_sequence")
+        except AttributeError:
+            self.__validate_npdu_sequence(value)
+            self.__npdu_sequence = tuple(value)
+        else:
+            raise ReassignmentError("You cannot change value of 'npdu_sequence' attribute once it is assigned.")
+
+    @property
+    def addressing(self) -> AddressingType:
+        """Addressing type which was used to transmit this message."""
+        return self.npdu_sequence[0].addressing
+
+    @property
+    def direction(self) -> TransmissionDirection:
+        """Information whether this message was received or sent by the code."""
+        return self.npdu_sequence[0].direction
+
+    @property  # noqa: F841
+    def transmission_start(self) -> TimeStamp:
+        """
+        Time stamp when transmission of this messages was initiated.
+
+        It is determined by a moment of time when the first N_PDU (that carried this message) was either published
+        to a bus (either received or transmitted).
+
+        :return: Time stamp when transmission of this message was initiated.
+        """
+        return self.npdu_sequence[0].transmission_time
+
+    @property  # noqa: F841
+    def transmission_end(self) -> TimeStamp:
+        """
+        Time stamp when transmission of this messages was completed.
+
+        It is determined by a moment of time when the last N_PDU (that carried this message) was either published
+        to a bus (either received or transmitted).
+
+        :return: Time stamp when transmission of this message was completed.
+        """
+        return self.npdu_sequence[-1].transmission_time
--- uds/utilities/common_types.py
+++ uds/utilities/common_types.py
@@ -1,8 +1,10 @@
 """Container with all types (and its aliases) used by the module."""
 
-__all__ = ["TimeMilliseconds", "RawByte", "RawBytes", "RawBytesTuple", "validate_raw_bytes"]
+__all__ = ["RawByte", "RawBytes", "RawBytesTuple", "validate_raw_bytes",
+           "TimeMilliseconds", "TimeStamp"]
 
 from typing import Union, Tuple, List, Any
+from datetime import datetime
 
 RawByte = int
 # TODO: prospector supports only pylint version 2.5 that has serious problem with Aliases in Python 3.9.
@@ -10,6 +12,7 @@ RawByte = int
 RawBytesTuple = Tuple[RawByte, ...]  # pylint: disable=unsubscriptable-object
 RawBytes = Union[RawBytesTuple, List[RawByte]]  # pylint: disable=unsubscriptable-object
 TimeMilliseconds = Union[int, float]  # noqa: F841, pylint: disable=unsubscriptable-object
+TimeStamp = datetime
 
 
 def validate_raw_bytes(value: Any) -> None:
