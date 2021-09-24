7555f43e26b83bc272b95702d2f12585a6fa0ecd
Maciej Dąbrowski <51504507+mdabrowski1990@users.noreply.github.com>
2021-09-24 15:39:29 +0200

    first_proposal
    Proposal of Segmenter API.

--- tests/software_tests/segmentation/test_abstract_segmenter.py
+++ tests/software_tests/segmentation/test_abstract_segmenter.py
@@ -1,113 +1,212 @@
 import pytest
-from mock import Mock
+from mock import Mock, patch, MagicMock
 
 from uds.segmentation.abstract_segmenter import AbstractSegmenter, SegmentationError, \
     UdsMessage, UdsMessageRecord
+from uds.messages import AbstractUdsPacket, AbstractUdsPacketRecord
 
 
 class TestAbstractSegmenter:
     """Tests for `AbstractSegmenter` class."""
 
+    SCRIPT_PATH = "uds.segmentation.abstract_segmenter"
+
     def setup(self):
         self.mock_abstract_segmenter = Mock(spec=AbstractSegmenter)
 
-    # _validate_packet
-
-    @pytest.mark.parametrize("packet", [None, True, "a packet", 232, [0x1, 0x2, 0x3], Mock()])
-    def test_validate_packet__valid(self, packet):
-        self.mock_abstract_segmenter.is_supported_packet.return_value = True
-        assert AbstractSegmenter._validate_packet(self=self.mock_abstract_segmenter, packet=packet) is None
-        self.mock_abstract_segmenter.is_supported_packet.assert_called_once_with(value=packet)
-
-    @pytest.mark.parametrize("packet", [None, True, "a packet", 232, [0x1, 0x2, 0x3], Mock()])
-    def test_validate_packet__type_error(self, packet):
-        self.mock_abstract_segmenter.is_supported_packet.return_value = False
-        with pytest.raises(TypeError):
-            AbstractSegmenter._validate_packet(self=self.mock_abstract_segmenter, packet=packet)
-        self.mock_abstract_segmenter.is_supported_packet.assert_called_once_with(value=packet)
-
-    # _validate_packets_sequence
-
-    @pytest.mark.parametrize("packets", [(True, False), [Mock(), Mock()], (1, 2, 3)])
-    def test_validate_packets_sequence__valid(self, packets):
-        self.mock_abstract_segmenter.is_supported_packets_sequence.return_value = True
-        assert AbstractSegmenter._validate_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is None
-        self.mock_abstract_segmenter.is_supported_packets_sequence.assert_called_once_with(value=packets)
-
-    @pytest.mark.parametrize("packets", [(True, False), [Mock(), Mock()], (1, 2, 3)])
-    def test_validate_packets_sequence__value_error(self, packets):
-        self.mock_abstract_segmenter.is_supported_packets_sequence.return_value = False
-        with pytest.raises(ValueError):
-            AbstractSegmenter._validate_packets_sequence(self=self.mock_abstract_segmenter, packets=packets)
-        self.mock_abstract_segmenter.is_supported_packets_sequence.assert_called_once_with(value=packets)
-
     # is_supported_packet
 
-    # TODO: moar tests :)
+    @pytest.mark.parametrize("result", [True, False])
+    @pytest.mark.parametrize("value", [None, 5, "some value", Mock()])
+    @patch(f"{SCRIPT_PATH}.isinstance")
+    def test_is_supported_packet(self, mock_isinstance, value, result):
+        mock_isinstance.return_value = result
+        assert AbstractSegmenter.is_supported_packet(self=self.mock_abstract_segmenter, value=value) is result
+        mock_isinstance.assert_called_once_with(value, self.mock_abstract_segmenter.supported_packet_classes)
 
-#     # segmentation
-#
-#     @pytest.mark.parametrize("message", [None, False, Mock(spec=UdsMessageRecord), (0x1, 0x2, 0x3)])
-#     def test_segmentation__type_error(self, message):
-#         with pytest.raises(TypeError):
-#             AbstractSegmenter.segmentation(self=self.mock_abstract_segmenter, message=message)
-#
-#     def test_segmentation__valid_input(self):
-#         AbstractSegmenter.segmentation(self=self.mock_abstract_segmenter, message=Mock(spec=UdsMessage))
-#
-#     # desegmentation
-#
-#     @pytest.mark.parametrize("packets", [None, "some packets", [1, 2, 3]])
-#     def test_desegmentation__segmentation_error(self, packets):
-#         self.mock_abstract_segmenter.is_complete_packet_set = Mock(return_value=False)
-#         with pytest.raises(SegmentationError):
-#             AbstractSegmenter.desegmentation(self=self.mock_abstract_segmenter, packets=packets)
-#         self.mock_abstract_segmenter.is_complete_packet_set.assert_called_once_with(packets=packets)
-#
-#     @pytest.mark.parametrize("packets", [None, "some packets", [1, 2, 3]])
-#     def test_desegmentation__valid_input(self, packets):
-#         self.mock_abstract_segmenter.is_complete_packet_set = Mock(return_value=True)
-#         AbstractSegmenter.desegmentation(self=self.mock_abstract_segmenter, packets=packets)
-#         self.mock_abstract_segmenter.is_complete_packet_set.assert_called_once_with(packets=packets)
-#
-#     # is_complete_packet_set
-#
-#     @pytest.mark.parametrize("packets", [None, False, 505, Mock(), "not packets"])
-#     def test_is_complete_packet_set__type_error(self, packets):
-#         with pytest.raises(TypeError):
-#             AbstractSegmenter.is_complete_packet_set(self=self.mock_abstract_segmenter, packets=packets)
-#
-#     @pytest.mark.parametrize("packets", [
-#         [Mock(spec=AbstractUdsPacket)],
-#         [Mock(spec=AbstractUdsPacketRecord)],
-#         (Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord)),
-#         (Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)),
-#     ])
-#     def test_is_complete_packet_set__valid_input(self, packets):
-#         AbstractSegmenter.is_complete_packet_set(self=self.mock_abstract_segmenter, packets=packets)
-#
-#     # is_first_packet
-#
-#     @pytest.mark.parametrize("packet", [None, True, "not a packet", 232, Mock(), [0x1, 0x2, 0x3]])
-#     def test_is_first_packet__type_error(self, packet):
-#         with pytest.raises(TypeError):
-#             AbstractSegmenter.is_first_packet(self=self.mock_abstract_segmenter, packet=packet)
-#
-#     @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacketRecord)])
-#     def test_is_first_packet__valid_input(self, packet):
-#         AbstractSegmenter.is_first_packet(self=self.mock_abstract_segmenter, packet=packet)
-#
-#     # get_consecutive_packets_needed
-#
-#     @pytest.mark.parametrize("packet", [None, True, "a packet", 6321, [0x1, 0x2, 0x3], Mock(spec=AbstractUdsPacket)])
-#     def test_get_consecutive_packets_needed__value_error(self, packet):
-#         self.mock_abstract_segmenter.is_first_packet.return_value = False
-#         with pytest.raises(ValueError):
-#             AbstractSegmenter.get_consecutive_packets_number(self=self.mock_abstract_segmenter, first_packet=packet)
-#         self.mock_abstract_segmenter.is_first_packet.assert_called_once_with(packet=packet)
-#
-#     @pytest.mark.parametrize("packet", [None, True, "a packet", 6321, [0x1, 0x2, 0x3], Mock(spec=AbstractUdsPacket)])
-#     def test_get_consecutive_packets_needed__valid_input(self, packet):
-#         self.mock_abstract_segmenter.is_first_packet.return_value = True
-#         AbstractSegmenter.get_consecutive_packets_number(self=self.mock_abstract_segmenter, first_packet=packet)
-#         self.mock_abstract_segmenter.is_first_packet.assert_called_once_with(packet=packet)
+    # is_supported_packets_sequence
+
+    @pytest.mark.parametrize("value", [None, True, 1, Mock(), {1, 2, 3}])
+    def test_is_supported_packets_sequence__false__invalid_type(self, value):
+        assert AbstractSegmenter.is_supported_packets_sequence(self=self.mock_abstract_segmenter, value=value) is False
+        self.mock_abstract_segmenter.is_supported_packet.assert_not_called()
+
+    @pytest.mark.parametrize("value", [
+        (1, 2, 3, 4),
+        (1, 2.1, 3, 4.4),
+        [2.2, 3.3, 4.4],
+        [None, True, False],
+        (True, False),
+    ])
+    def test_is_supported_packets_sequence__false__invalid_elements_type(self, value):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = False
+        assert AbstractSegmenter.is_supported_packets_sequence(self=self.mock_abstract_segmenter, value=value) is False
+        self.mock_abstract_segmenter.is_supported_packet.assert_called_once()
+
+    @pytest.mark.parametrize("value", [
+        (1, 2.1, 3, 4.4),
+        [None, True, False],
+    ])
+    def test_is_supported_packets_sequence__false__more_element_types(self, value):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = True
+        assert AbstractSegmenter.is_supported_packets_sequence(self=self.mock_abstract_segmenter, value=value) is False
+        self.mock_abstract_segmenter.is_supported_packet.assert_called()
+
+    def test_is_supported_packets_sequence__false__empty_sequence(self):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = True
+        assert AbstractSegmenter.is_supported_packets_sequence(self=self.mock_abstract_segmenter, value=[]) is False
+
+    @pytest.mark.parametrize("value", [
+        (1, 2, 3, 4),
+        [2.2, 3.3, 4.4],
+        (True, False),
+    ])
+    def test_is_supported_packets_sequence__true(self, value):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = True
+        assert AbstractSegmenter.is_supported_packets_sequence(self=self.mock_abstract_segmenter, value=value) is True
+        self.mock_abstract_segmenter.is_supported_packet.assert_called()
+
+    # is_initial_packet
+
+    @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)])
+    def test_is_initial_packet__type_error(self, packet):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = False
+        with pytest.raises(TypeError):
+            AbstractSegmenter.is_initial_packet(self=self.mock_abstract_segmenter, packet=packet)
+        self.mock_abstract_segmenter.is_supported_packet.assert_called_once_with(packet)
+
+    @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)])
+    def test_is_initial_packet__false(self, packet):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = True
+        mock_initial_packet_types = MagicMock()
+        mock_initial_packet_types.__contains__.return_value = False
+        self.mock_abstract_segmenter.initial_packet_types = mock_initial_packet_types
+        assert AbstractSegmenter.is_initial_packet(self=self.mock_abstract_segmenter, packet=packet) is False
+        self.mock_abstract_segmenter.is_supported_packet.assert_called_once_with(packet)
+
+    @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)])
+    def test_is_initial_packet__true(self, packet):
+        self.mock_abstract_segmenter.is_supported_packet.return_value = True
+        mock_initial_packet_types = MagicMock()
+        mock_initial_packet_types.__contains__.return_value = True
+        self.mock_abstract_segmenter.initial_packet_types = mock_initial_packet_types
+        assert AbstractSegmenter.is_initial_packet(self=self.mock_abstract_segmenter, packet=packet) is True
+        self.mock_abstract_segmenter.is_supported_packet.assert_called_once_with(packet)
+
+    # get_consecutive_packets_number
+
+    @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacketRecord)])
+    def test_get_consecutive_packets_number__value_error(self, packet):
+        self.mock_abstract_segmenter.is_initial_packet.return_value = False
+        with pytest.raises(ValueError):
+            AbstractSegmenter.get_consecutive_packets_number(self=self.mock_abstract_segmenter, first_packet=packet)
+        self.mock_abstract_segmenter.is_initial_packet.assert_called_once_with(packet)
+
+    @pytest.mark.parametrize("packet", [Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacketRecord)])
+    def test_get_consecutive_packets_number__valid(self, packet):
+        self.mock_abstract_segmenter.is_initial_packet.return_value = True
+        AbstractSegmenter.get_consecutive_packets_number(self=self.mock_abstract_segmenter, first_packet=packet)
+        self.mock_abstract_segmenter.is_initial_packet.assert_called_once_with(packet)
+
+    # is_following_packets_sequence
+
+    @pytest.mark.parametrize("packets", [
+        (1, 2, 3, 4),
+        (1, 2.1, 3, 4.4),
+        [2.2, 3.3, 4.4],
+        [None, True, False],
+        (True, False),
+    ])
+    def test_is_following_packets_sequence__value_error(self, packets):
+        self.mock_abstract_segmenter.is_supported_packets_sequence.return_value = False
+        with pytest.raises(ValueError):
+            AbstractSegmenter.is_following_packets_sequence(self=self.mock_abstract_segmenter, packets=packets)
+        self.mock_abstract_segmenter.is_supported_packets_sequence.assert_called_once_with(packets)
+
+    @pytest.mark.parametrize("packets", [
+        (1, 2, 3, 4),
+        (1, 2.1, 3, 4.4),
+        [2.2, 3.3, 4.4],
+        [None, True, False],
+        (True, False),
+    ])
+    def test_is_following_packets_sequence__false(self, packets):
+        self.mock_abstract_segmenter.is_supported_packets_sequence.return_value = True
+        self.mock_abstract_segmenter.is_initial_packet.return_value = False
+        assert AbstractSegmenter.is_following_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is False
+        self.mock_abstract_segmenter.is_initial_packet.assert_called_once_with(packets[0])
+
+    @pytest.mark.parametrize("packets", [
+        (1, 2, 3, 4),
+        (1, 2.1, 3, 4.4),
+        [2.2, 3.3, 4.4],
+        [None, True, False],
+        (True, False),
+    ])
+    def test_is_following_packets_sequence__valid(self, packets):
+        self.mock_abstract_segmenter.is_supported_packets_sequence.return_value = True
+        self.mock_abstract_segmenter.is_initial_packet.return_value = True
+        assert AbstractSegmenter.is_following_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is None
+        self.mock_abstract_segmenter.is_supported_packets_sequence.assert_called_once_with(packets)
+
+    # is_complete_packets_sequence
+
+    @pytest.mark.parametrize("packets", [
+        (Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)),
+        [Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord)],
+    ])
+    def test_is_complete_packets_sequence__not_a_sequence(self, packets):
+        self.mock_abstract_segmenter.is_following_packets_sequence.return_value = False
+        assert AbstractSegmenter.is_complete_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is False
+        self.mock_abstract_segmenter.is_following_packets_sequence.assert_called_once_with(packets)
+        self.mock_abstract_segmenter.get_consecutive_packets_number.assert_not_called()
+
+    @pytest.mark.parametrize("packets", [
+        (Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)),
+        [Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord)],
+    ])
+    def test_is_complete_packets_sequence__invalid_packets_number(self, packets):
+        mock_eq_false = MagicMock()
+        mock_eq_false.__eq__.return_value = False
+        self.mock_abstract_segmenter.is_following_packets_sequence.return_value = True
+        self.mock_abstract_segmenter.get_consecutive_packets_number.return_value = mock_eq_false
+        assert AbstractSegmenter.is_complete_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is False
+        self.mock_abstract_segmenter.is_following_packets_sequence.assert_called_once_with(packets)
+        self.mock_abstract_segmenter.get_consecutive_packets_number.assert_called_once_with(packets[0])
+        mock_eq_false.__eq__.assert_called_once_with(len(packets))
+
+    @pytest.mark.parametrize("packets", [
+        (Mock(spec=AbstractUdsPacket), Mock(spec=AbstractUdsPacket)),
+        [Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord), Mock(spec=AbstractUdsPacketRecord)],
+    ])
+    def test_is_complete_packets_sequence__true(self, packets):
+        self.mock_abstract_segmenter.is_following_packets_sequence.return_value = True
+        self.mock_abstract_segmenter.get_consecutive_packets_number.return_value = len(packets)
+        assert AbstractSegmenter.is_complete_packets_sequence(self=self.mock_abstract_segmenter, packets=packets) is True
+        self.mock_abstract_segmenter.is_following_packets_sequence.assert_called_once_with(packets)
+        self.mock_abstract_segmenter.get_consecutive_packets_number.assert_called_once_with(packets[0])
+
+    # segmentation
+
+    @pytest.mark.parametrize("message", [None, False, Mock(spec=UdsMessageRecord), (0x1, 0x2, 0x3)])
+    def test_segmentation__type_error(self, message):
+        with pytest.raises(TypeError):
+            AbstractSegmenter.segmentation(self=self.mock_abstract_segmenter, message=message)
+
+    def test_segmentation__valid_input(self):
+        AbstractSegmenter.segmentation(self=self.mock_abstract_segmenter, message=Mock(spec=UdsMessage))
+
+    # desegmentation
+
+    @pytest.mark.parametrize("packets", [None, "some packets", [1, 2, 3]])
+    def test_desegmentation__segmentation_error(self, packets):
+        self.mock_abstract_segmenter.is_complete_packets_sequence.return_value = False
+        with pytest.raises(SegmentationError):
+            AbstractSegmenter.desegmentation(self=self.mock_abstract_segmenter, packets=packets)
+        self.mock_abstract_segmenter.is_complete_packets_sequence.assert_called_once_with(packets)
+
+    @pytest.mark.parametrize("packets", [None, "some packets", [1, 2, 3]])
+    def test_desegmentation__valid_input(self, packets):
+        self.mock_abstract_segmenter.is_complete_packets_sequence.return_value = True
+        AbstractSegmenter.desegmentation(self=self.mock_abstract_segmenter, packets=packets)
+        self.mock_abstract_segmenter.is_complete_packets_sequence.assert_called_once_with(packets)
--- uds/segmentation/abstract_segmenter.py
+++ uds/segmentation/abstract_segmenter.py
@@ -23,30 +23,6 @@ class AbstractSegmenter(ABC):
     Each concrete segmenter class handles a single bus.
     """
 
-    def _validate_packet(self, packet: PacketTyping) -> None:
-        """
-        Validate whether the argument contains an object of a supported packet type.
-
-        :param packet: Packet object to check.
-
-        :raise TypeError: Provided value is not an object of a supported packet type.
-        """
-        if not self.is_supported_packet(value=packet):
-            raise TypeError(f"Provided value is not a packet object that is supported by this Segmenter. "
-                            f"Actual value: {packet}.")
-
-    def _validate_packets_sequence(self, packets: PacketsSequence) -> None:
-        """
-        Validate whether the argument contains a packet sequence of a supported type.
-
-        :param packets: Packet sequence to check.
-
-        :raise ValueError: Provided value is not a packets sequence of a supported type.
-        """
-        if not self.is_supported_packets_sequence(value=packets):
-            raise ValueError(f"Provided value is not a packets sequence of a type supported by this Segmenter. "
-                             f"Actual value: {packets}.")
-
     @property
     @abstractmethod
     def supported_packet_classes(self) -> Tuple[type]:
@@ -66,12 +42,6 @@ class AbstractSegmenter(ABC):
         :return: True if provided value is an object of a supported packet type, False otherwise.
         """
         return isinstance(value, self.supported_packet_classes)
-        # TODO: tests and code
-
-
-        # TODO: tests and code
-        # if not self._is_supported_packet(value=packet):
-        #     raise TypeError(f"Provided value is not supported packet type. Actual value: {packet}.")
 
     def is_supported_packets_sequence(self, value: Any) -> bool:
         """
@@ -81,10 +51,14 @@ class AbstractSegmenter(ABC):
 
         :return: True if provided value is a packet sequence of a supported type, False otherwise.
         """
-        # TODO: tests and code
-
-
-        # TODO: tests and code
+        if not isinstance(value, (list, tuple)):
+            # not a sequence
+            return False
+        if not all(self.is_supported_packet(element) for element in value):
+            # at least one element is not a packet of a supported type
+            return False
+        # check if all packets are the same type
+        return len({type(element) for element in value}) == 1
 
     def is_initial_packet(self, packet: PacketTyping) -> bool:
         """
@@ -92,35 +66,54 @@ class AbstractSegmenter(ABC):
 
         :param packet: Packet to check.
 
-        :raise TypeError: TODO
+        :raise TypeError: Provided value is not an object of a supported packet type.
 
         :return: True if the packet is the only or the first packet of a diagnostic message.
         """
-        # TODO: tests and code
+        if not self.is_supported_packet(packet):
+            raise TypeError(f"Provided value is not a packet object that is supported by this Segmenter. "
+                            f"Actual value: {packet}.")
+        return packet.packet_type in self.initial_packet_types
 
     @abstractmethod
-    def get_consecutive_packets_number(self, first_packet: PacketTyping) -> int:  # type: ignore
+    def get_consecutive_packets_number(self, first_packet: PacketTyping) -> int:
         """
         Get number of consecutive packets that must follow this packet to fully store a diagnostic message.
 
         :param first_packet: The first packet of a segmented diagnostic message.
 
+        :raise ValueError: Provided value is not an an initial packet.
+
         :return: Number of following packets that together carry a diagnostic message.
         """
-        # TODO: tests and code
+        if not self.is_initial_packet(first_packet):
+            raise ValueError(f"Provided value is not an initial packet of a type supported by this Segmenter. "
+                             f"Actual value: {first_packet}.")
 
     @abstractmethod
     def is_following_packets_sequence(self, packets: PacketsSequence) -> bool:
         """
-        Check whether provided packets are a sequence of following packets that might form a diagnostic message.
+        Check whether provided packets are a sequence of following packets.
+
+        Note: This function will return True under following conditions:
+         - a sequence of packets was provided
+         - the first packet in the sequence is an initial packet
+         - no other packet in the sequence is an initial packet
+         - each packet (except the first one) is a consecutive packet for the previous packet in the sequence
 
         :param packets: Packets sequence to check.
 
-        :return: True if the packets are
-        """
+        :raise ValueError: Provided value is not a packets sequence of a supported type.
 
-    @abstractmethod
-    def is_complete_packets_sequence(self, packets: PacketsSequence) -> bool:  # type: ignore
+        :return: True if the provided packets are a sequence of following packets, otherwise False.
+        """
+        if not self.is_supported_packets_sequence(packets):
+            raise ValueError(f"Provided value is not a packets sequence of a supported type."
+                             f"Actual value: {packets}.")
+        if not self.is_initial_packet(packets[0]):
+            return False
+
+    def is_complete_packets_sequence(self, packets: PacketsSequence) -> bool:
         """
         Check whether provided packets are full sequence of packets that form exactly one diagnostic message.
 
@@ -129,10 +122,11 @@ class AbstractSegmenter(ABC):
         :return: True if the packets form exactly one diagnostic message.
             False if there are missing, additional or inconsistent (e.g. two packets that initiate a message) packets.
         """
-        # TODO: tests and code
+        return self.is_following_packets_sequence(packets) and \
+            self.get_consecutive_packets_number(packets[0]) == len(packets)
 
     @abstractmethod
-    def segmentation(self, message: UdsMessage) -> PacketsDefinitionTuple:  # type: ignore
+    def segmentation(self, message: UdsMessage) -> PacketsDefinitionTuple:
         """
         Perform diagnostic message segmentation.
 
@@ -146,14 +140,16 @@ class AbstractSegmenter(ABC):
             raise TypeError(f"Provided value is not UdsMessage type. Actual value: {message}.")
 
     @abstractmethod
-    def desegmentation(self, packets: PacketsSequence) -> Union[UdsMessage, UdsMessageRecord]:  # type: ignore
+    def desegmentation(self, packets: PacketsSequence) -> Union[UdsMessage, UdsMessageRecord]:
         """
         Perform diagnostic message desegmentation.
 
         :param packets: UDS packets to desegment into UDS message.
 
-        :raise SegmentationError: Provided packets are not a complete packets set that form a diagnostic message.
+        :raise SegmentationError: Provided packets are not a complete packet sequence that form a diagnostic message.
 
         :return: Diagnostic message created from received packets.
         """
-        # TODO: tests and code
+        if not self.is_complete_packets_sequence(packets):
+            raise SegmentationError(f"Provided packets are not a complete that form a diagnostic message. "
+                                    f"Actual value: {packets}.")
