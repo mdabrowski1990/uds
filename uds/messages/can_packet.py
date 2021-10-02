80f20114edb85fb49b0fe72766f85a1ca8510f30
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-10-02 14:44:46 +0200

    Create can_packet.py
    Note what shall be implemented.

---
+++ uds/messages/can_packet.py
@@ -0,0 +1,17 @@
+"""TODO"""
+
+__all__ = ["CanPacketType", "CanPacket", "CanPacketRecord"]
+
+from .uds_packet import AbstractUdsPacketType, AbstractUdsPacket, AbstractUdsPacketRecord
+
+
+class CanPacketType(AbstractUdsPacketType):
+    """TODO: knowledge base first"""
+
+
+class CanPacket(AbstractUdsPacket):
+    """TODO: knowledge base first"""
+
+
+class CanPacketRecord(AbstractUdsPacketRecord):
+    """TODO: knowledge base first"""
