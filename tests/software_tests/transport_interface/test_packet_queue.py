fd1fce4df3b323bd91880072a5ef90db8552a806
Maciej Dąbrowski <51504507+mdabrowski1990@users.noreply.github.com>
2021-12-10 15:39:09 +0100

    design_queue_api
    First proposal

--- tests/software_tests/transport_interface/test_packet_queue.py
+++ tests/software_tests/transport_interface/test_packet_queue.py
@@ -1,55 +1,93 @@
 import pytest
 from mock import Mock
 
-from uds.transport_interface.packet_queue import ReceivedPacketsQueue
+from uds.transport_interface.packet_queue import AbstractPacketsQueue, PacketsQueue, TimestampedPacketsQueue
 
 
-class TestReceivedPacketsQueue:
-    """Unit tests for `ReceivedPacketsQueue class."""
+class TestAbstractPacketsQueue:
+    """Unit tests for `AbstractPacketsQueue` class."""
 
     def setup(self):
-        self.mock_received_packets_queue = Mock(spec=ReceivedPacketsQueue)
+        self.mock_abstract_packets_queue = Mock(spec=AbstractPacketsQueue)
 
     # __init__
 
     def test_init(self):
         with pytest.raises(NotImplementedError):
-            ReceivedPacketsQueue.__init__(self=self.mock_received_packets_queue, packet_class=Mock())
+            AbstractPacketsQueue.__init__(self=self.mock_abstract_packets_queue, packet_class=Mock())
 
     # __del__
 
     def test_del(self):
         with pytest.raises(NotImplementedError):
-            ReceivedPacketsQueue.__del__(self=self.mock_received_packets_queue)
+            AbstractPacketsQueue.__del__(self=self.mock_abstract_packets_queue)
 
     # __len__
 
     def test_len(self):
         with pytest.raises(NotImplementedError):
-            ReceivedPacketsQueue.__len__(self=self.mock_received_packets_queue)
+            AbstractPacketsQueue.__len__(self=self.mock_abstract_packets_queue)
 
     # is_empty
 
     def test_is_empty(self):
         with pytest.raises(NotImplementedError):
-            ReceivedPacketsQueue.is_empty(self=self.mock_received_packets_queue)
+            AbstractPacketsQueue.is_empty(self=self.mock_abstract_packets_queue)
 
-    # packet_task_done
+    # one_task_done
 
-    def test_packet_task_done(self):
+    def test_one_task_done(self):
         with pytest.raises(NotImplementedError):
-            ReceivedPacketsQueue.packet_task_done(self=self.mock_received_packets_queue)
+            AbstractPacketsQueue.one_task_done(self=self.mock_abstract_packets_queue)
 
-    # get_packet
+    # block
+
+    def test_get_block(self):
+        with pytest.raises(NotImplementedError):
+            AbstractPacketsQueue.block(self=self.mock_abstract_packets_queue)
+
+    # clear
+
+    def test_get_clear(self):
+        with pytest.raises(NotImplementedError):
+            AbstractPacketsQueue.clear(self=self.mock_abstract_packets_queue)
+
+
+class TestTimestampedPacketsQueue:
+    """Unit tests for `TimestampedPacketsQueue` class."""
+
+    def setup(self):
+        self.mock_timestamped_packets_queue = Mock(spec=TimestampedPacketsQueue)
 
     @pytest.mark.asyncio
     async def test_get_packet(self):
         with pytest.raises(NotImplementedError):
-            await ReceivedPacketsQueue.get_packet(self=self.mock_received_packets_queue)
+            await TimestampedPacketsQueue.get_packet(self=self.mock_timestamped_packets_queue)
 
     # put_packet
 
     @pytest.mark.asyncio
     async def test_put_packet(self):
         with pytest.raises(NotImplementedError):
-            await ReceivedPacketsQueue.put_packet(self=self.mock_received_packets_queue, packet=Mock())
+            await TimestampedPacketsQueue.put_packet(self=self.mock_timestamped_packets_queue, packet=Mock())
+
+
+class TestPacketsQueue:
+    """Unit tests for `PacketsQueue` class."""
+
+    def setup(self):
+        self.mock_fifo_packets_queue = Mock(spec=PacketsQueue)
+
+    # get_packet
+
+    @pytest.mark.asyncio
+    async def test_get_packet(self):
+        with pytest.raises(NotImplementedError):
+            await PacketsQueue.get_packet(self=self.mock_fifo_packets_queue)
+
+    # put_packet
+
+    @pytest.mark.asyncio
+    async def test_put_packet(self):
+        with pytest.raises(NotImplementedError):
+            await PacketsQueue.put_packet(self=self.mock_fifo_packets_queue, packet=Mock())
--- uds/transport_interface/packet_queue.py
+++ uds/transport_interface/packet_queue.py
@@ -1,34 +1,38 @@
 """Implementation of UDS Packets queues."""
 
-__all__ = ["ReceivedPacketsQueue"]
+__all__ = ["PacketsQueue", "TimestampedPacketsQueue"]
 
-from typing import NoReturn
+from typing import NoReturn, Optional
+from abc import ABC, abstractmethod
+from queue import Queue
 
-from uds.packet import AbstractUdsPacketRecord
+from uds.utilities import TimeStamp
+from uds.packet import PacketAlias
 
 
-class ReceivedPacketsQueue:
-    """Queue for storing received packets."""
+class AbstractPacketsQueue(ABC):
+    """Abstract definition of a queue with UDS packets."""
 
-    def __init__(self, packet_class: type = AbstractUdsPacketRecord) -> None:  # noqa: F841
+    def __init__(self, packet_class: type) -> None:  # noqa: F841
         """
-        Create a queue as a storage for received packets.
+        Create a queue as a storage for UDS packets.
 
         :param packet_class: A class that defines UDS packets type that is accepted by this queue.
             One can use this parameter to restrict packets managed by this queue.
 
         :raise TypeError: Provided packet_class argument is not a class that inherits after
-            :class:"uds.packet.abstract_packet.AbstractUdsPacketRecord".
+            :class:"~uds.packet.abstract_packet.AbstractUdsPacket" or
+            :class:"~uds.packet.abstract_packet.AbstractUdsPacketRecord".
         """
         raise NotImplementedError
 
     def __del__(self) -> NoReturn:
         """
-        Delete the object safely.
+        Delete the queue safely.
 
-        To satisfy safe closure or tasks using the queue:
+        To satisfy safe closure of tasks using the queue:
          - prevent new tasks creations
-         - close or await already started tasks
+         - await (till timeout) for closure of already started tasks
         """
         raise NotImplementedError
 
@@ -44,15 +48,51 @@ class ReceivedPacketsQueue:
         """
         raise NotImplementedError
 
-    def packet_task_done(self) -> None:
+    def one_task_done(self) -> None:
         """
         Inform that a task related to one packet was completed.
 
-        This method is used during closing all tasks safely and quietly.
+        This method is used for monitoring tasks so they can be completed safely and closed quietly.
         """
         raise NotImplementedError
 
-    async def get_packet(self) -> AbstractUdsPacketRecord:
+    def block(self) -> None:
+        """Block from putting new packets to the queue until all packets are gotten and processed."""
+        raise NotImplementedError
+
+    def clear(self) -> None:
+        """Delete all packets stored by the queue."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_packet(self) -> PacketAlias:
+        """
+        Get the next received packet from the queue.
+
+        Note: If called, when there are no packets in the queue, then execution would await until another packet
+            is received.
+
+        :return: The next received packet.
+        """
+
+    @abstractmethod
+    async def put_packet(self, packet: PacketAlias) -> None:
+        """
+        Add a packet (that was just received) to the end of the queue.
+
+        :param packet: A packet that was just received.
+        """
+
+    @property  # noqa: F841
+    @abstractmethod
+    def _queue_object(self) -> Queue:
+        """Primitive queue object that is wrapped."""
+
+
+class TimestampedPacketsQueue:
+    """Priority queue with UDS packets ordered by packet's timestamp."""
+
+    async def get_packet(self) -> PacketAlias:
         """
         Get the next received packet from the queue.
 
@@ -63,7 +103,31 @@ class ReceivedPacketsQueue:
         """
         raise NotImplementedError
 
-    async def put_packet(self, packet: AbstractUdsPacketRecord) -> None:  # noqa: F841
+    async def put_packet(self, packet: PacketAlias, timestamp: Optional[TimeStamp] = None) -> None:  # noqa: F841
+        """
+        Add a packet (that was just received) to the end of the queue.
+
+        :param packet: A packet that was just received.
+        :param timestamp:
+        """
+        raise NotImplementedError
+
+
+class PacketsQueue:
+    """FIFO queue for UDS packets."""
+
+    async def get_packet(self) -> PacketAlias:
+        """
+        Get the next received packet from the queue.
+
+        Note: If called, when there are no packets in the queue, then execution would await until another packet
+            is received.
+
+        :return: The next received packet.
+        """
+        raise NotImplementedError
+
+    async def put_packet(self, packet: PacketAlias) -> None:
         """
         Add a packet (that was just received) to the end of the queue.
 
