05fac8280d1ef87d71a9f657c1452842b53cbff5
Maciej Dąbrowski <maciek_dabrowski@o2.pl>
2021-12-29 18:22:50 +0100

    add_docstrings
    Define API for Transport Interface..
    Provide annotations and docstrings.

---
+++ tests/software_tests/transport_interface/test_abstract_transport_interface.py
@@ -0,0 +1,64 @@
+import pytest
+from mock import Mock
+
+from uds.transport_interface.abstract_transport_interface import AbstractTransportInterface
+
+
+class TestAbstractTransportInterface:
+    """Unit tests for `AbstractTransportInterface` class."""
+
+    def setup(self):
+        self.mock_transport_interface = Mock(spec=AbstractTransportInterface)
+
+    # __init__
+
+    def test_init(self):
+        with pytest.raises(NotImplementedError):
+            AbstractTransportInterface.__init__(self=self.mock_transport_interface,
+                                                bus_handler=Mock())
+
+    # bus_handler
+
+    def test_bus_handler(self):
+        with pytest.raises(NotImplementedError):
+            AbstractTransportInterface.bus_handler.fget(self.mock_transport_interface)
+
+    # packet_records
+
+    def test_packet_records(self):
+        with pytest.raises(NotImplementedError):
+            AbstractTransportInterface.packet_records.fget(self.mock_transport_interface)
+
+    # message_records
+
+    def test_message_records(self):
+        with pytest.raises(NotImplementedError):
+            AbstractTransportInterface.message_records.fget(self.mock_transport_interface)
+
+    # await_packet_received
+
+    @pytest.mark.asyncio
+    async def test_await_packet_received(self):
+        with pytest.raises(NotImplementedError):
+            await AbstractTransportInterface.await_packet_received(self=self.mock_transport_interface)
+
+    # await_packet_transmitted
+
+    @pytest.mark.asyncio
+    async def test_await_packet_transmitted(self):
+        with pytest.raises(NotImplementedError):
+            await AbstractTransportInterface.await_packet_transmitted(self=self.mock_transport_interface)
+
+    # await_message_transmitted
+
+    @pytest.mark.asyncio
+    async def test_await_message_transmitted(self):
+        with pytest.raises(NotImplementedError):
+            await AbstractTransportInterface.await_message_transmitted(self=self.mock_transport_interface)
+
+    # await_message_received
+
+    @pytest.mark.asyncio
+    async def test_await_message_received(self):
+        with pytest.raises(NotImplementedError):
+            await AbstractTransportInterface.await_message_received(self=self.mock_transport_interface)
--- uds/transport_interface/abstract_transport_interface.py
+++ uds/transport_interface/abstract_transport_interface.py
@@ -4,12 +4,16 @@ __all__ = ["AbstractTransportInterface"]
 
 from typing import Optional, Tuple, Any
 from abc import ABC, abstractmethod
-from datetime import datetime
 
 from uds.utilities import TimeMilliseconds
 from uds.packet import AbstractUdsPacket, AbstractUdsPacketRecord
 from uds.message import UdsMessage, UdsMessageRecord
-from .packet_queues import TimestampedPacketsQueue, PacketsQueue
+
+
+DEFAULT_PACKET_RECORDS_STORED: int = 100
+"""Default size of container for UDS packet records in Transport Interface."""
+DEFAULT_MESSAGE_RECORDS_STORED: int = 10
+"""Default size of container for UDS message records in Transport Interface."""
 
 
 class AbstractTransportInterface(ABC):
@@ -21,41 +25,109 @@ class AbstractTransportInterface(ABC):
 
     def __init__(self,
                  bus_handler: Any,
-                 max_packet_records_stored: int,  # TODO: define and add default value
-                 max_message_records_stored: int  # TODO: define and add default value
-                 ) -> None:
-        ...
+                 max_packet_records_stored: int = DEFAULT_PACKET_RECORDS_STORED,
+                 max_message_records_stored: int = DEFAULT_MESSAGE_RECORDS_STORED) -> None:
+        """
+        Create Transport Interface (an object for handling UDS Transport and Network layers).
 
-    @abstractmethod  # TODO: assess whether abstract
-    def send_packet(self, packet: AbstractUdsPacket, delay: Optional[TimeMilliseconds] = None) -> None:
-        ...
-
-    @abstractmethod  # TODO: assess whether abstract
-    def send_message(self, message: UdsMessage, delay: Optional[TimeMilliseconds] = None) -> None:
-        ...
+        :param bus_handler: An object that handles the bus (Physical and Data layers of OSI Model).
+        :param max_packet_records_stored: Maximal number of UDS packet records to be stored in
+            :attr:`~uds.transport_interface.abstract_transport_interface.AbstractTransportInterface.packet_records`.
+        :param max_message_records_stored: Maximal number of UDS message records to be stored in
+            :attr:`~uds.transport_interface.abstract_transport_interface.AbstractTransportInterface.message_records`.
+        """
+        raise NotImplementedError
 
     @property
-    @abstractmethod  # TODO: assess whether abstract
+    def bus_handler(self) -> Any:
+        """An object which handles the bus (Physical and Data layers of OSI Model) for this Transport Interface."""
+        raise NotImplementedError
+
+    @property
     def packet_records(self) -> Tuple[AbstractUdsPacketRecord, ...]:
-        ...
+        """Container with records of UDS packets that was either received or transmitted on the bus."""
+        raise NotImplementedError
 
     @property
-    @abstractmethod  # TODO: assess whether abstract
     def message_records(self) -> Tuple[UdsMessageRecord, ...]:
-        ...
+        """Container with records of UDS Messages that was either received or transmitted on the bus."""
+        raise NotImplementedError
 
-    @abstractmethod  # TODO: assess whether abstract
+    @staticmethod
+    @abstractmethod
+    def is_supported_bus_handler(bus_handler: Any) -> bool:
+        """
+        Check whether provided value is a bus handler that is supported by this Transport Interface.
+
+        :param bus_handler: Value to check.
+
+        :return: True if provided object can handle the bus for this Transport Interface, False otherwise.
+        """
+
     async def await_packet_received(self, timeout: Optional[TimeMilliseconds] = None) -> AbstractUdsPacketRecord:
-        ...
+        """
+        Wait until the next UDS packet is received.
 
-    @abstractmethod  # TODO: assess whether abstract
+        :param timeout: Maximal time (in milliseconds) to wait.
+
+        :raise TimeoutError: Timeout was reached.
+
+        :return: Record with historic information of a packet that was just received.
+        """
+        raise NotImplementedError
+
     async def await_packet_transmitted(self, timeout: Optional[TimeMilliseconds] = None) -> AbstractUdsPacketRecord:
-        ...
+        """
+        Wait until the next UDS packet is transmitted.
 
-    @abstractmethod  # TODO: assess whether abstract
+        :param timeout: Maximal time (in milliseconds) to wait.
+
+        :raise TimeoutError: Timeout was reached.
+
+        :return: Record with historic information of a packet that was just transmitted.
+        """
+        raise NotImplementedError
+
     async def await_message_transmitted(self, timeout: Optional[TimeMilliseconds] = None) -> UdsMessageRecord:
-        ...
+        """
+        Wait until the next UDS message is received.
 
-    @abstractmethod  # TODO: assess whether abstract
+        :param timeout: Maximal time (in milliseconds) to wait.
+
+        :raise TimeoutError: Timeout was reached.
+
+        :return: Record with historic information of a message that was just received.
+        """
+        raise NotImplementedError
+
     async def await_message_received(self, timeout: Optional[TimeMilliseconds] = None) -> UdsMessageRecord:
-        ...
+        """
+        Wait until the next UDS message is transmitted.
+
+        :param timeout: Maximal time (in milliseconds) to wait.
+
+        :raise TimeoutError: Timeout was reached.
+
+        :return: Record with historic information of a message that was just transmitted.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def send_packet(self, packet: AbstractUdsPacket, delay: Optional[TimeMilliseconds] = None) -> None:
+        """
+        Transmit UDS packet on the configured bus.
+
+        :param packet: A packet to send.
+        :param delay: Value of a delay (in milliseconds) if the transmission to be scheduled in the future.
+            None if the transmission to be executed immediately.
+        """
+
+    @abstractmethod
+    def send_message(self, message: UdsMessage, delay: Optional[TimeMilliseconds] = None) -> None:
+        """
+        Transmit UDS message on the configured bus.
+
+        :param message: A message to send.
+        :param delay: Value of a delay (in milliseconds) if the transmission to be scheduled in the future.
+            None if the transmission to be executed immediately.
+        """
