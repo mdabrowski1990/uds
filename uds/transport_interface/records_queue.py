629fa0f1bc275cba8bb9af3d13324b3750f50575
Maciej Dąbrowski <51504507+mdabrowski1990@users.noreply.github.com>
2022-02-01 14:30:53 +0100

    docstrings
    Refine methods names and add docstrings.

--- uds/transport_interface/records_queue.py
+++ uds/transport_interface/records_queue.py
@@ -1,4 +1,4 @@
-"""Implementation of records queues."""
+"""Implementation of queues with historic records."""
 
 __all__ = ["RecordsQueue"]
 
@@ -9,32 +9,56 @@ from uds.packet import AbstractUdsPacket
 
 
 class RecordsQueue:
-    # TODO: docstring
+    """Queue with historic records of UDS Packets or Messages."""
 
     RecordsTypeAlias = Union[Type[UdsMessageRecord], Type[AbstractUdsPacket]]
-    # TODO: docstring
+    """Alias of a record type that is accepted by this Queue."""
     RecordAlias = Union[UdsMessageRecord, AbstractUdsPacket]
+    """Alias of a record stored by this Queue."""
 
     def __init__(self, records_type: RecordsTypeAlias, history_size: int) -> None:
-        ...
+        """
+        Create FIFO queue with historic records of UDS Packets or Messages.
+
+        :param records_type: Type of records to store.
+        :param history_size: Number of records to store.
+        """
 
     @property
     def records_type(self) -> RecordsTypeAlias:
-        ...
+        """Type of records stored by this queue."""
 
     @property
-    def max_history_size(self) -> int:
-        ...
+    def history_size(self) -> int:
+        """
+        Number of records stored by this queue.
+
+        .. note:: If a record beyond this number is received, then the oldest one is pushed out of the queue.
+        """
+
+    @property
+    def total_records_number(self) -> int:
+        """Total number of records that went through the queue."""
 
     @property
     def records_history(self) -> Tuple[RecordAlias, ...]:
-        ...
+        """Historic records from the youngest to the oldest."""
 
-    def put(self, record: RecordAlias) -> None:
-        ...
+    def put_record(self, record: RecordAlias) -> None:
+        """
+        Add a new record to the queue.
 
-    async def get_next(self) -> RecordAlias:
-        ...
+        :param record: A new record to add.
 
-    def clear(self) -> None:
-        ...
+        :raise TypeError: Provided record value has unexpected type.
+        """
+
+    async def get_next_record(self) -> RecordAlias:
+        """
+        Get the next record to enter the queue.
+
+        :return: The next record that was put to the queue.
+        """
+
+    def clear_records_history(self) -> None:
+        """Clear all stored records."""
--- uds/transport_interface/transmission_queue.py
+++ uds/transport_interface/transmission_queue.py
@@ -1,4 +1,4 @@
-# TODO: docstring
+"""Implementation of a queue with PDUs to transmit."""
 
 __all__ = ["TransmissionQueue"]
 
@@ -9,35 +9,60 @@ from uds.packet import AbstractUdsPacket
 
 
 class TransmissionQueue:
-    # TODO: docstring
+    """Queue with PDUs to transmit."""
 
-    PDUAlias = Union[UdsMessage, AbstractUdsPacket]
-    # TODO: docstring
     PDUTypeAlias = Union[Type[UdsMessage], Type[AbstractUdsPacket]]
-    # TODO: docstring
+    """Alias of a PDU type that is accepted by this Queue."""
+    PDUAlias = Union[UdsMessage, AbstractUdsPacket]
+    """Alias of a PDU (either a message or a packet) stored by this Queue."""
 
     def __init__(self, pdu_type: PDUTypeAlias) -> None:
-        ...
+        """
+        Created timestamped queue with PDUs to transmit.
+
+        :param pdu_type: Type of PDUs to store.
+        """
 
     def __len__(self) -> int:
-        ...
+        """Number of elements currently contained."""
 
     @property
     def pdu_type(self) -> PDUTypeAlias:
-        ...
+        """Type of PDUs stored by this queue."""
 
     @property
     def is_empty(self) -> bool:
-        ...
+        """Flag whether the queue is empty (does not contain any PDUs)."""
 
-    def mark_task_done(self):
-        ...
+    def mark_pdu_sent(self):
+        """
+        Inform that one PDU transmission was completed or aborted.
+
+        This method is used for monitoring PDU transmission tasks, so they can be completed safely and closed quietly.
+        """
 
     def clear(self) -> None:
-        ...
+        """Delete all PDUs stored by the queue."""
 
     async def get_pdu(self) -> PDUAlias:
-        ...
+        """
+        Get the next PDU from the queue.
+
+        .. note:: If called when there are no packets available in the queue, then the method would await until
+            the next packet is ready (timestamp is achieved).
+
+        :raise RuntimeError: Internal implementation problem occurred.
+
+        :return: The next PDU from the queue.
+        """
 
     def put_pdu(self, pdu: PDUAlias, timestamp: Optional[float] = None) -> None:
-        ...
+        """
+        Add a PDU to the queue.
+
+        :param pdu: A PDU to add to the queue.
+        :param timestamp: Timestamp value (from perf_counter) when make the packet available (gettable) in the queue.
+
+        :raise TypeError: Provided PDU or timestamp value has unexpected type.
+        """
+    
