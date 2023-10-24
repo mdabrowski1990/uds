60a7e2e933d360b442384e6d92a9bcb10b33c6dd
Maciej Dabrowski <maciej.dabrowski@software-quality.pl>
2023-10-24 15:22:48 +0200

    Update __init__.py

--- uds/utilities/__init__.py
+++ uds/utilities/__init__.py
@@ -1,10 +1,9 @@
 """Various helper functions, classes and variables that are shared and reused within the project."""
 
 from .enums import ValidatedEnum, ExtendableEnum, ByteEnum, NibbleEnum
-from .common_types import TimeMillisecondsAlias,\
+from .common_types import TimeMillisecondsAlias, \
     RawBytesAlias, RawBytesTupleAlias, RawBytesListAlias, RawBytesSetAlias, \
     validate_nibble, validate_raw_bytes, validate_raw_byte
 from .bytes_operations import Endianness, int_to_bytes_list, bytes_list_to_int
-from .custom_exceptions import ReassignmentError, InconsistentArgumentsError, AmbiguityError, \
-    UnusedArgumentError
+from .custom_exceptions import ReassignmentError, InconsistentArgumentsError, AmbiguityError, UnusedArgumentError
 from .custom_warnings import UnusedArgumentWarning, ValueWarning
