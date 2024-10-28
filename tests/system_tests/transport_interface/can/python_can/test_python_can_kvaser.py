b7a0ca6aa83f84bb1243d70d10eded729b8d14d2
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2024-10-28 16:43:44 +0100

    Update test_python_can_kvaser.py

--- tests/system_tests/transport_interface/can/python_can/test_python_can_kvaser.py
+++ tests/system_tests/transport_interface/can/python_can/test_python_can_kvaser.py
@@ -13,6 +13,7 @@ class KvaserConfig(AbstractPythonCanTests):
     """Configuration for Python CAN Transport Interface tests with Kvaser CAN interfaces."""
 
     def setup_class(self):
+        """Configure CAN bus that is managed by Kvaser CAN interfaces."""
         self.can_interface_1 = Bus(interface="kvaser", channel=0, fd=True, receive_own_messages=True)
         self.can_interface_2 = Bus(interface="kvaser", channel=1, fd=True, receive_own_messages=True)
 
