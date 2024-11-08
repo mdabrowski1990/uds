23742f28d89367e0c90759068c2b4caf38f6b5f1
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2024-11-08 16:35:59 +0100

    Update __init__.py

--- uds/database/__init__.py
+++ uds/database/__init__.py
@@ -5,3 +5,4 @@ Tools for decoding and encoding information from/to diagnostic messages.
 """
 
 from .abstract_data_record import AbstractDataRecord, DataRecordType, DecodedDataRecord
+from .abstract_service import AbstractService
