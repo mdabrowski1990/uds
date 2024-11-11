351526db63b703537cb39667d9494986b8b7f0aa
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2024-11-11 17:33:28 +0100

    Separate `services` module in database

--- uds/database/__init__.py
+++ uds/database/__init__.py
@@ -5,4 +5,4 @@ Tools for decoding and encoding information from/to diagnostic messages.
 """
 
 from .abstract_data_record import AbstractDataRecord, DataRecordType, DecodedDataRecord
-from .abstract_service import AbstractService
+from .services import AbstractService
