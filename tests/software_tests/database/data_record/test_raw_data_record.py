7547ea034635dd841c83c05bc4f0966fd5e038ea
Przemyslaw Niescior <niesciorprzemek@wp.pl>
2024-11-12 21:32:30 +0100

    improving unit tests for RawDataRecord

--- tests/software_tests/database/data_record/test_raw_data_record.py
+++ tests/software_tests/database/data_record/test_raw_data_record.py
@@ -1,26 +1,88 @@
 import pytest
+from mock import patch
 
-from uds.database.data_record import RawDataRecord, DecodedDataRecord
+from uds.database.data_record.raw_data_record import RawDataRecord
 
-
-def test_raw_data_record_initialization():
-    record = RawDataRecord(name="TestRawDataRecord", length=16)
-    assert record.name == "TestRawDataRecord"
-    assert record.length == 16
-    assert record.data_record_type == "RawDataRecord"
-    assert record.is_reoccurring is False
-    assert record.min_occurrences == 1
-    assert record.max_occurrences == 1
-    assert record.contains == ()
-    assert record.decode(1234) == DecodedDataRecord(name="TestRawDataRecord", raw_value=1234, physical_value=1234)
-    assert record.encode(1234) == 1234
+SCRIPT_LOCATION = "uds.database.data_record.raw_data_record"
 
 
-def test_raw_data_record_invalid_name():
-    with pytest.raises(TypeError):
-        RawDataRecord(name=123, length=16)
+class TestRawDataRecord:
 
+    def setup_method(self):
+        self.name = "TestRawDataRecord"
+        self.length = 8
+        self.raw_data_record = RawDataRecord(self.name, self.length)
 
-def test_raw_data_record_invalid_length():
-    with pytest.raises(ValueError):
-        RawDataRecord(name="TestRecord", length=-1)
+    # __init__
+
+    def test_init_valid(self):
+        assert self.raw_data_record.name == self.name
+        assert self.raw_data_record.length == self.length
+
+    def test_init_type_error_for_name(self):
+        with pytest.raises(TypeError):
+            RawDataRecord(123, self.length)
+
+    def test_init_value_error_for_length(self):
+        with pytest.raises(ValueError):
+            RawDataRecord(self.name, -5)
+
+    def test_init_type_error_for_length(self):
+        with pytest.raises(TypeError):
+            RawDataRecord(self.name, "eight")
+
+    # length
+
+    def test_length_getter(self):
+        assert self.raw_data_record.length == self.length
+
+    def test_length_setter_valid(self):
+        self.raw_data_record.length = 16
+        assert self.raw_data_record.length == 16
+
+    def test_length_setter_type_error(self):
+        with pytest.raises(TypeError):
+            self.raw_data_record.length = "sixteen"
+
+    def test_length_setter_value_error(self):
+        with pytest.raises(ValueError):
+            self.raw_data_record.length = -10
+
+    # is_reoccurring
+
+    def test_is_reoccurring(self):
+        assert self.raw_data_record.is_reoccurring is False
+
+    # min_occurrences
+
+    def test_min_occurrences(self):
+        assert self.raw_data_record.min_occurrences == 1
+
+    # max_occurrences
+
+    def test_max_occurrences(self):
+        assert self.raw_data_record.max_occurrences == 1
+
+    # contains
+
+    def test_contains(self):
+        assert self.raw_data_record.contains == ()
+
+    # decode
+
+    @patch(f"{SCRIPT_LOCATION}.DecodedDataRecord")
+    def test_decode(self, mock_decoded_data_record):
+        raw_value = 42
+        result = self.raw_data_record.decode(raw_value)
+        mock_decoded_data_record.assert_called_once_with(
+            name=self.name,
+            raw_value=raw_value,
+            physical_value=raw_value
+        )
+        assert result == mock_decoded_data_record.return_value
+
+    # encode
+
+    def test_encode(self):
+        physical_value = 42
+        assert self.raw_data_record.encode(physical_value) == physical_value
