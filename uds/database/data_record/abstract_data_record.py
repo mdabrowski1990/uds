a2ee48287d37e1b9127ea020304e528579a0fbac
Maciej Dąbrowski <51504507+mdabrowski1990@users.noreply.github.com>
2024-12-16 08:10:36 +0100

    API for Databases (#298)
    - Abstract Database class defined
    - tiny fix for Abstract Service (distinguishing between request and response message)
    - adjust aliases of Data Records

--- uds/database/data_record/abstract_data_record.py
+++ uds/database/data_record/abstract_data_record.py
@@ -1,12 +1,53 @@
 """Definition of AbstractDataRecord which is a base class for all Data Records."""
 
-__all__ = ["AbstractDataRecord", "DataRecordPhysicalValueAlias", "DecodedDataRecord"]
+__all__ = ["AbstractDataRecord", "DataRecordPhysicalValueAlias", "DecodedDataRecord", "DataRecordValueAlias"]
 
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple, TypedDict, Union
+from typing import Dict, Optional, Sequence, Tuple, TypedDict, Union
 
-DataRecordPhysicalValueAlias = Union[int, float, str, Tuple["DecodedDataRecord", ...]]
-"""Alias of Data Records' physical value."""
+DataRecordValueAlias = Union[
+    int,  # raw value
+    float,  # physical value calculated through formula
+    str,  # text (physical) value from either Text Table or Text encoding
+    Dict[str, "DataRecordValueAlias"],  # value of container's children
+    Sequence[Union[int, Dict[str, "DataRecordValueAlias"]]],  # values for reoccurring container
+]
+"""
+Alias of Data Records' input value.
+
+Each type represent other data:
+
+- int type - raw value of a data record
+- float type - physical value of a formula data record
+
+    .. warning:: Providing physical value as float might sometime cause issues due
+        `floating-point precision <https://docs.python.org/3/tutorial/floatingpoint.html>`_.
+        The closest raw value would be evaluated and put into a payload.
+
+        To avoid rounding, provide raw value (int type).
+
+- str type - text (physical) value of either text table or text data record
+- dict type - values for children of a container data records
+- sequence type - values for following occurrences of a container data record
+"""
+
+DataRecordPhysicalValueAlias = Union[
+    int,  # physical value is the same as raw value
+    float,  # physical value calculated through formula
+    str,  # decoded text value
+    Tuple[Tuple["DecodedDataRecord", ...], ...]  # decoded container value, each element is another record
+]
+"""
+Alias of Data Records' physical value.
+
+Each type represent other data:
+
+- int type - physical value is the same as raw value
+- float type - value received through formula calculation
+- str type - text value received either through encoding (e.g. ASCII, UTF-8)
+  or mapping (each value has specific meaning)
+- tuple type - one element for each container occurrence; each element is a tuple with values for children data records
+"""
 
 
 class DecodedDataRecord(TypedDict):
---
+++ uds/database/service/abstract_service.py
@@ -0,0 +1,75 @@
+"""Definition of UDS Service data encoding and decoding."""
+
+__all__ = ["AbstractService"]
+
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+from uds.message import RequestSID, ResponseSID
+from uds.utilities import RawBytesAlias, RawBytesListAlias
+
+from ..data_record import DataRecordValueAlias, DecodedDataRecord
+
+
+class AbstractService(ABC):
+    """Common interface for all diagnostic services."""
+
+    @property  # noqa: F841
+    @abstractmethod
+    def request_sid(self) -> RequestSID:
+        """Service Identifier in request messages."""
+
+    @property  # noqa: F841
+    @abstractmethod
+    def response_sid(self) -> ResponseSID:
+        """Service Identifier in (positive) response messages."""
+
+    @abstractmethod
+    def decode(self, payload: RawBytesAlias) -> List[DecodedDataRecord]:
+        """
+        Decode physical values carried in payload of a diagnostic message.
+
+        :param payload: Payload of a diagnostic message.
+
+        :return: Decoded Data Records values from provided diagnostic message.
+        """
+
+    def encode(self,
+               sid: Union[int, RequestSID, ResponseSID],
+               **data_records_values: DataRecordValueAlias) -> RawBytesListAlias:
+        """
+        Encode diagnostic message payload for this service.
+
+        :param sid: Value of Service Identifier. It should be either equal to either `request_sid` or `response_sid`.
+        :param data_records_values: Value for each data record that is part of a service message.
+
+        :raise ValueError: Provided `sid` value is neither equal to request SID value nor response SID value for this
+            diagnostic service.
+
+        :return: Payload of a diagnostic message.
+        """
+        if sid == self.request_sid:
+            return self.encode_request(**data_records_values)
+        if sid == self.response_sid:
+            return self.encode_response(**data_records_values)
+        raise ValueError("Provided SID value is neither request or response SID value for this service.")
+
+    @abstractmethod
+    def encode_request(self, **data_records_values: DataRecordValueAlias) -> RawBytesListAlias:
+        """
+        Encode diagnostic message payload for this service's request message.
+
+        :param data_records_values: Value for each data record that is part of a service message.
+
+        :return: Payload of a request diagnostic message.
+        """
+
+    @abstractmethod
+    def encode_response(self, **data_records_values: DataRecordValueAlias) -> RawBytesListAlias:
+        """
+        Encode diagnostic message payload for this service's response message.
+
+        :param data_records_values: Value for each data record that is part of a service message.
+
+        :return: Payload of a response diagnostic message.
+        """
