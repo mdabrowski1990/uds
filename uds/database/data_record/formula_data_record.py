737ef458dff7b9b9984c153592bae05b3cc298c0
Przemyslaw Niescior <niesciorprzemek@wp.pl>
2025-04-06 13:28:53 +0200

    and addressing review comments

--- uds/database/data_record/formula_data_record.py
+++ uds/database/data_record/formula_data_record.py
@@ -1,10 +1,9 @@
-"""Definition of FormulaDataRecord."""
+"""Definitions of CustomFormulaDataRecord and LinearFormulaDataRecord."""
 
 from dataclasses import dataclass
-from typing import Callable, Optional, Union
+from typing import Callable, Optional, Union, Tuple
 
-from .abstract_data_record import DataRecordPhysicalValueAlias, DecodedDataRecord
-from .raw_data_record import RawDataRecord
+from .abstract_data_record import AbstractDataRecord
 
 
 @dataclass
@@ -19,7 +18,7 @@ class FormulaRange:
             raise ValueError("Attribute 'min_value' must be less than 'max_value'.")
 
 
-class CustomFormulaDataRecord(RawDataRecord):
+class CustomFormulaDataRecord(AbstractDataRecord):
     def __init__(
             self,
             name: str,
@@ -28,7 +27,7 @@ class CustomFormulaDataRecord(RawDataRecord):
             encode_formula: Callable,
             formula_range: Optional[FormulaRange] = None
     ) -> None:
-        super().__init__(name=name, length=length)
+        super().__init__(name=name)
 
         if not callable(decode_formula):
             raise TypeError("Provided 'decode_formula' is not callable.")
@@ -39,24 +38,107 @@ class CustomFormulaDataRecord(RawDataRecord):
                 "Parameter 'formula_range' must be type of 'FormulaRange', "
                 f"provided type: '{type(formula_range).__name__}'."
             )
+        self.length = length
         self.decode_formula = decode_formula
         self.encode_formula = encode_formula
-        self.formula_range = formula_range
+        self.formula_range = formula_range or FormulaRange(min_value=0, max_value=self.max_raw_value)
 
-    def decode(self, raw_value: int) -> Union[int, DecodedDataRecord]:
-        decoded_data: DecodedDataRecord = super().decode(raw_value)
-        if self.formula_range and self.formula_range.min_value < decoded_data.raw_value < self.formula_range.max_value:
-            decoded_data.physical_value = self.decode_formula(decoded_data.raw_value)
-        return decoded_data
+    # TODO: Should we move the length setter to AbstractDataRecord? Maybe even set values of some other properties?
+    @property
+    def length(self) -> int:
+        """Get number of bits that this Data Record is stored over."""
+        return self.__length
 
-    def encode(self, physical_value: DataRecordPhysicalValueAlias) -> int:
-        encoded_data: int = super().encode(physical_value)
-        if self.formula_range and self.formula_range.min_value < encoded_data < self.formula_range.max_value:
-            return self.encode_formula(encoded_data)
-        return int(encoded_data)
+    @length.setter
+    def length(self, value: int) -> None:
+        """
+        Set the length, ensuring it's an integer and within an acceptable range.
+
+        :param value: Number of bits that this Data Record is stored over.
+
+        :raise TypeError: Provided value is not int type.
+        :raise ValueError: Provided value is less or equal 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("Length must be an integer.")
+        if value <= 0:
+            raise ValueError("Length must be a positive integer.")
+        self.__length = value
+
+    @property  # noqa: F841
+    def is_reoccurring(self) -> bool:
+        """
+        Whether this Data Record might occur multiple times.
+
+        Values meaning:
+        - False - exactly one occurrence in every diagnostic message
+        - True - number of occurrences might vary
+        """
+        return False
+
+    @property  # noqa: F841
+    def min_occurrences(self) -> int:
+        """
+        Minimal number of this Data Record occurrences.
+
+        .. note:: Relevant only if :attr:`~uds.database.data_record.raw_data_record.RawDataRecord.is_reoccurring`
+            equals True.
+        """
+        return 1
+
+    @property  # noqa: F841
+    def max_occurrences(self) -> Optional[int]:
+        """
+        Maximal number of this Data Record occurrences.
+
+        .. note:: Relevant only if :attr:`~uds.database.data_record.raw_data_record.RawDataRecord.is_reoccurring`
+            equals True.
+        .. warning:: No maximal number (infinite number of occurrences) is represented by None value.
+        """
+        return 1
+
+    @property  # noqa: F841
+    def contains(self) -> Tuple[AbstractDataRecord, ...]:
+        """Get Data Records contained by this Data Record."""
+        return ()
+
+    def decode(self, raw_value: int) -> Union[int, float]:
+        """
+        Decode the physical value of the given `raw_value` using the decode formula provided by the user.
+
+        :param raw_value: Raw value to be decoded.
+
+        :return: Physical value decoded with custom decode formula.
+
+        :raises ValueError: Provided `raw_value` is out of range.
+        """
+        # TODO: Should we implement all the validation, or should we leave it to the user?
+        if not self.formula_range.min_value <= raw_value <= self.formula_range.max_value:
+            raise ValueError("Provided raw_value parameter is out of range: "
+                             f"must be between {self.formula_range.min_value} "
+                             f"and {self.formula_range.max_value}, got {raw_value}.")
+        return self.decode_formula(raw_value)
+
+    def encode(self, physical_value: Union[int, float]) -> int:
+        """
+        Encode the raw value of the given `physical_value` using the encode formula provided by the user.
+
+        :param physical_value: Physical value to be encoded.
+
+        :return: Raw value encoded with custom encode formula.
+
+        :raises ValueError: Encoded `raw_value` of provided `physical_value` is out of range.
+        """
+        raw_value = self.encode_formula(physical_value)
+        # TODO: Do I think correctly that FormulaRange should refer only to raw_value?
+        if not self.formula_range.min_value <= raw_value <= self.formula_range.max_value:
+            raise ValueError("Encoded raw_value is out of configured formula_range: "
+                             f"must be between {self.formula_range.min_value} "
+                             f"and {self.formula_range.max_value}, got {raw_value}.")
+        return raw_value
 
 
-class LinearFormulaDataRecord(RawDataRecord):
+class LinearFormulaDataRecord(CustomFormulaDataRecord):
 
     def __init__(
             self,
@@ -64,19 +146,46 @@ class LinearFormulaDataRecord(RawDataRecord):
             length: int,
             factor: float,
             offset: float,
+            formula_range: FormulaRange = None,
     ) -> None:
+        self._decode_formula = lambda x: (x / factor) + offset
+        self._encode_formula = lambda x: (x - offset) * factor
         super().__init__(
             name=name,
             length=length,
+            decode_formula=self._decode_formula,
+            encode_formula=self._encode_formula,
+            formula_range=formula_range
         )
-        self._decode_formula = lambda x: (x / factor) + offset
-        self._encode_formula = lambda x: (x - offset) * factor
 
-    def decode(self, raw_value: int) -> Union[int, DecodedDataRecord]:
-        decoded_data: DecodedDataRecord = super().decode(raw_value)
-        decoded_data.physical_value = self._decode_formula(decoded_data.raw_value)
-        return decoded_data
+    def decode(self, raw_value: int) -> Union[int, float]:
+        """
+        Decode the physical value of the given `raw_value` using the linear decode formula.
 
-    def encode(self, physical_value: DataRecordPhysicalValueAlias) -> int:
-        encoded_data: int = super().encode(physical_value)
-        return int(self._encode_formula(encoded_data))
+        :param raw_value: Raw value to be decoded.
+
+        :return: Physical value decoded with linear decode formula.
+
+        :raises TypeError: Provided `raw_value` is not int type.
+        :raises ValueError: Provided `raw_value` is out of range.
+        """
+        if not isinstance(raw_value, int):
+            raise TypeError(f"Expected raw_value to be an int type, got '{type(raw_value).__name__}' instead.")
+        return super().decode(raw_value)
+
+    def encode(self, physical_value: Union[int, float]) -> int:
+        """
+        Encode the raw value of the given `physical_value` using the linear encode formula.
+
+        :param physical_value: Physical value to be encoded.
+
+        :return: Raw value encoded with linear encode formula.
+
+        :raises TypeError: Provided `physical_value` is not int type.
+        :raises ValueError: Encoded `raw_value` of provided `physical_value` is out of range.
+        """
+        if not isinstance(physical_value, int):
+            raise TypeError(
+                f"Expected physical_value to be an int type, got '{type(physical_value).__name__}' instead."
+            )
+        return super().encode(physical_value)
