f28b61c65d7de9d0a4763a66994ff1d5ca7d3401
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2025-11-14 16:08:39 +0100

    Update test_input_output_control_by_identifier.py
    integration tests for 2020

--- tests/software_tests/translator/service_definitions/test_input_output_control_by_identifier.py
+++ tests/software_tests/translator/service_definitions/test_input_output_control_by_identifier.py
@@ -500,11 +500,408 @@ class TestInputOutputControlByIdentifier2020Integration:
     """Integration tests for `InputOutputControlByIdentifier` service version 2020."""
 
     @pytest.mark.parametrize("payload, decoded_message", [
+        # returnControlToECU (0x00)
+        (
+            [0x2F, 0xFF, 0x01, 0x00],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'SID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x2F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': "ReservedForISO15765-5",
+                    'raw_value': 0xFF01,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "returnControlToECU",
+                    'raw_value': 0x00,
+                    'unit': None
+                },
+            )
+        ),
+        (
+            [0x6F, 0x04, 0x63, 0x00, 0x64, 0x4D, 0x4E, 0xA7, 0xA5, 0xE3, 0x84, 0x1F],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'RSID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x6F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0x0463,
+                    'raw_value': 0x0463,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "returnControlToECU",
+                    'raw_value': 0x00,
+                    'unit': None
+                },
+                {
+                    'children': ((), (), (), (), (), (), (), ()),
+                    'length': 8,
+                    'name': 'controlState',
+                    'physical_value': (0x64, 0x4D, 0x4E, 0xA7, 0xA5, 0xE3, 0x84, 0x1F),
+                    'raw_value': (0x64, 0x4D, 0x4E, 0xA7, 0xA5, 0xE3, 0x84, 0x1F),
+                    'unit': None
+                },
+            )
+        ),
+        # resetToDefault (0x01)
+        (
+            [0x2F, 0x6B, 0xC0, 0x01],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'SID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x2F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0x6BC0,
+                    'raw_value': 0x6BC0,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "resetToDefault",
+                    'raw_value': 0x01,
+                    'unit': None
+                },
+            )
+        ),
+        (
+            [0x6F, 0x92, 0xDD, 0x01, 0x53, 0x4B, 0xC2, 0xA4, 0xD8, 0x41, 0x2F, 0x72, 0x24],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'RSID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x6F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0x92DD,
+                    'raw_value': 0x92DD,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "resetToDefault",
+                    'raw_value': 0x01,
+                    'unit': None
+                },
+                {
+                    'children': ((), (), (), (), (), (), (), (), ()),
+                    'length': 8,
+                    'name': 'controlState',
+                    'physical_value': (0x53, 0x4B, 0xC2, 0xA4, 0xD8, 0x41, 0x2F, 0x72, 0x24),
+                    'raw_value': (0x53, 0x4B, 0xC2, 0xA4, 0xD8, 0x41, 0x2F, 0x72, 0x24),
+                    'unit': None
+                },
+            )
+        ),
+        # freezeCurrentState (0x02)
+        (
+            [0x2F, 0xAE, 0xE7, 0x02],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'SID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x2F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0xAEE7,
+                    'raw_value': 0xAEE7,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "freezeCurrentState",
+                    'raw_value': 0x02,
+                    'unit': None
+                },
+            )
+        ),
+        (
+            [0x6F, 0x93, 0x85, 0x02, 0x61, 0x39, 0x8E, 0xDF, 0xA7, 0x80, 0x4A, 0x9B, 0x26, 0xB6, 0x4A, 0x9D, 0x46,
+             0x95, 0xFA, 0x56, 0xEC, 0xAD, 0x3E, 0xD4],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'RSID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x6F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0x9385,
+                    'raw_value': 0x9385,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "freezeCurrentState",
+                    'raw_value': 0x02,
+                    'unit': None
+                },
+                {
+                    'children': ((), (), (), (), (), (), (), (), (), (), (), (), (), (), (), (), (), (), (), ()),
+                    'length': 8,
+                    'name': 'controlState',
+                    'physical_value': (0x61, 0x39, 0x8E, 0xDF, 0xA7, 0x80, 0x4A, 0x9B, 0x26, 0xB6, 0x4A, 0x9D, 0x46,
+                                       0x95, 0xFA, 0x56, 0xEC, 0xAD, 0x3E, 0xD4),
+                    'raw_value': (0x61, 0x39, 0x8E, 0xDF, 0xA7, 0x80, 0x4A, 0x9B, 0x26, 0xB6, 0x4A, 0x9D, 0x46,
+                                  0x95, 0xFA, 0x56, 0xEC, 0xAD, 0x3E, 0xD4),
+                    'unit': None
+                },
+            )
+        ),
+        # shortTermAdjustment (0x03)
+        (
+            [0x2F, 0xF1, 0x86, 0x03, 0x02],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'SID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x2F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 'ActiveDiagnosticSessionDataIdentifier',
+                    'raw_value': 0xF186,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "shortTermAdjustment",
+                    'raw_value': 0x03,
+                    'unit': None
+                },
+                {
+                    'children': (
+                        {
+                            'children': (),
+                            'length': 1,
+                            'name': 'reserved',
+                            'physical_value': 0,
+                            'raw_value': 0,
+                            'unit': None
+                        },
+                        {
+                            'children': (),
+                            'length': 7,
+                            'name': 'ActiveDiagnosticSession',
+                            'physical_value': 'programmingSession',
+                            'raw_value': 0x02,
+                            'unit': None
+                        },
+                    ),
+                    'length': 8,
+                    'name': 'controlState',
+                    'physical_value': 0x02,
+                    'raw_value': 0x02,
+                    'unit': None
+                },
+            )
+        ),
+        (
+            [0x6F, 0xE1, 0x66, 0x03, 0xBD],
+            (
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'RSID',
+                    'physical_value': 'InputOutputControlByIdentifier',
+                    'raw_value': 0x6F,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 16,
+                    'name': 'DID',
+                    'physical_value': 0xE166,
+                    'raw_value': 0xE166,
+                    'unit': None
+                },
+                {
+                    'children': (),
+                    'length': 8,
+                    'name': 'inputOutputControlParameter',
+                    'physical_value': "shortTermAdjustment",
+                    'raw_value': 0x03,
+                    'unit': None
+                },
+                {
+                    'children': ((),),
+                    'length': 8,
+                    'name': 'controlState',
+                    'physical_value': (0xBD,),
+                    'raw_value': (0xBD,),
+                    'unit': None
+                },
+            )
+        ),
     ])
     def test_decode(self, payload, decoded_message):
         assert INPUT_OUTPUT_CONTROL_BY_IDENTIFIER_2020.decode(payload) == decoded_message
 
     @pytest.mark.parametrize("data_records_values, sid, rsid, payload", [
+        # returnControlToECU (0x00)
+        (
+            {
+                "DID": 0x9F0A,
+                "inputOutputControlParameter": 0x00,
+            },
+            RequestSID.InputOutputControlByIdentifier,
+            None,
+            bytearray([0x2F, 0x9F, 0x0A, 0x00])
+        ),
+        (
+            {
+                "DID": 0x9328,
+                "inputOutputControlParameter": 0x00,
+                "controlState": (0x22, 0x36, 0x1D, 0x28, 0x8D, 0x4D, 0x74, 0x13, 0x3B, 0x4F, 0x1A, 0x77, 0x9F, 0x1F,
+                                 0xBB, 0x9B)
+            },
+            None,
+            ResponseSID.InputOutputControlByIdentifier,
+            bytearray([0x6F, 0x93, 0x28, 0x00,
+                       0x22, 0x36, 0x1D, 0x28, 0x8D, 0x4D, 0x74, 0x13, 0x3B, 0x4F, 0x1A, 0x77, 0x9F, 0x1F, 0xBB, 0x9B])
+        ),
+        # resetToDefault (0x01)
+        (
+            {
+                "DID": 0x6AB3,
+                "inputOutputControlParameter": 0x01,
+            },
+            RequestSID.InputOutputControlByIdentifier,
+            None,
+            bytearray([0x2F, 0x6A, 0xB3, 0x01])
+        ),
+        (
+            {
+                "DID": 0xAF1A,
+                "inputOutputControlParameter": 0x01,
+                "controlState": (0xEF,)
+            },
+            None,
+            ResponseSID.InputOutputControlByIdentifier,
+            bytearray([0x6F, 0xAF, 0x1A, 0x01,
+                       0xEF])
+        ),
+        # freezeCurrentState (0x02)
+        (
+            {
+                "DID": 0xDC64,
+                "inputOutputControlParameter": 0x02,
+            },
+            RequestSID.InputOutputControlByIdentifier,
+            None,
+            bytearray([0x2F, 0xDC, 0x64, 0x02])
+        ),
+        (
+            {
+                "DID": 0xE9ED,
+                "inputOutputControlParameter": 0x02,
+                "controlState": (0xAB, 0x7C, 0xC9, 0x26, 0x98, 0x3E, 0x4B, 0x6E, 0x69, 0x4D, 0x37, 0xD2, 0x2D, 0x70,
+                                 0xD3, 0xC8),
+            },
+            None,
+            ResponseSID.InputOutputControlByIdentifier,
+            bytearray([0x6F, 0xE9, 0xED, 0x02,
+                       0xAB, 0x7C, 0xC9, 0x26, 0x98, 0x3E, 0x4B, 0x6E, 0x69, 0x4D, 0x37, 0xD2, 0x2D, 0x70, 0xD3, 0xC8])
+        ),
+        # shortTermAdjustment (0x03)
+        (
+            {
+                "DID": 0x32CB,
+                "inputOutputControlParameter": 0x03,
+                "controlState": (0x6B, 0x65, 0x01, 0xAD),
+            },
+            RequestSID.InputOutputControlByIdentifier,
+            None,
+            bytearray([0x2F, 0x32, 0xCB, 0x03,
+                       0x6B, 0x65, 0x01, 0xAD])
+        ),
+        (
+            {
+                "DID": 0xF186,
+                "inputOutputControlParameter": 0x03,
+                "controlState": 0x60,
+                "controlEnableMask": 0x7F,
+            },
+            RequestSID.InputOutputControlByIdentifier,
+            None,
+            bytearray([0x2F, 0xF1, 0x86, 0x03,
+                       0x60,
+                       0x7F])
+        ),
+        (
+            {
+                "DID": 0xCDEA,
+                "inputOutputControlParameter": 0x03,
+                "controlState": (0x1E, 0xD7, 0x04, 0xF2),
+            },
+            None,
+            ResponseSID.InputOutputControlByIdentifier,
+            bytearray([0x6F, 0xCD, 0xEA, 0x03,
+                       0x1E, 0xD7, 0x04, 0xF2])
+        ),
     ])
     def test_encode(self, data_records_values, sid, rsid, payload):
         assert INPUT_OUTPUT_CONTROL_BY_IDENTIFIER_2020.encode(data_records_values=data_records_values,
