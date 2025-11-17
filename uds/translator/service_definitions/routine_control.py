a6add05cee5eed7ec2c3fe065a483776003d726c
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2025-11-17 11:22:17 +0100

    service defined

---
+++ uds/translator/service_definitions/routine_control.py
@@ -0,0 +1,34 @@
+"""Translation for RoutineControl (SID 0x31) service."""
+
+__all__ = ["ROUTINE_CONTROL"]
+
+from uds.message import RequestSID
+
+from ..data_record import ConditionalMappingDataRecord
+from ..data_record_definitions import (
+    OPTIONAL_ROUTINE_STATUS,
+    RID,
+    ROUTINE_CONTROL_OPTION,
+    ROUTINE_CONTROL_SUB_FUNCTION,
+    ROUTINE_INFO,
+    ROUTINE_STATUS,
+)
+from ..service import Service
+
+RESPONSE_CONTINUATION_MAPPING = {
+    0x01: (RID, ROUTINE_INFO, OPTIONAL_ROUTINE_STATUS),
+    0x02: (RID, ROUTINE_INFO, OPTIONAL_ROUTINE_STATUS),
+    0x03: (RID, ROUTINE_INFO, ROUTINE_STATUS),
+}
+
+CONDITIONAL_RESPONSE_CONTINUATION = ConditionalMappingDataRecord(
+    mapping=RESPONSE_CONTINUATION_MAPPING | {key + 0x80: value
+                                             for key, value in RESPONSE_CONTINUATION_MAPPING.items()})
+
+ROUTINE_CONTROL = Service(request_sid=RequestSID.RoutineControl,
+                          request_structure=(ROUTINE_CONTROL_SUB_FUNCTION,
+                                             RID,
+                                             ROUTINE_CONTROL_OPTION),
+                          response_structure=(ROUTINE_CONTROL_SUB_FUNCTION,
+                                              CONDITIONAL_RESPONSE_CONTINUATION))
+"""Default translator for :ref:`RoutineControl <knowledge-base-service-routine-control>` service."""
