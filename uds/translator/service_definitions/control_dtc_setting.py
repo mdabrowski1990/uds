ec84cd65e395879464cae8c2fe5f81c4c717b47b
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2025-11-25 15:56:43 +0100

    Update control_dtc_setting.py

--- uds/translator/service_definitions/control_dtc_setting.py
+++ uds/translator/service_definitions/control_dtc_setting.py
@@ -8,7 +8,7 @@ from ..data_record_definitions import CONTROL_DTC_SETTING_SUB_FUNCTION, DTC_SETT
 from ..service import Service
 
 CONTROL_DTC_SETTING = Service(request_sid=RequestSID.ControlDTCSetting,
-                                     request_structure=(CONTROL_DTC_SETTING_SUB_FUNCTION,
-                                                        DTC_SETTING_CONTROL_OPTION_RECORD),
-                                     response_structure=(CONTROL_DTC_SETTING_SUB_FUNCTION,))
+                              request_structure=(CONTROL_DTC_SETTING_SUB_FUNCTION,
+                                                 DTC_SETTING_CONTROL_OPTION_RECORD),
+                              response_structure=(CONTROL_DTC_SETTING_SUB_FUNCTION,))
 """Default translator for :ref:`ControlDTCSetting <knowledge-base-service-control-dtc-setting>` service."""
