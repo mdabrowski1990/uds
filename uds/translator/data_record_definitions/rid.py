9d6023aad4bb2df0cab3f762e9f4d7fb8edc1d87
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2025-12-21 19:55:10 +0100

    Rework RID related Data Records

--- uds/translator/data_record_definitions/rid.py
+++ uds/translator/data_record_definitions/rid.py
@@ -1,8 +1,10 @@
 """Data Records definitions for Routines and :ref:`Routine Identifiers <knowledge-base-rid>`."""
 
-__all__ = ["RID",
-           "ROUTINE_CONTROL_OPTION",
-           "ROUTINE_STATUS", "OPTIONAL_ROUTINE_STATUS"]
+__all__ = [
+    "RID",
+    "ROUTINE_CONTROL_OPTION",
+    "ROUTINE_STATUS", "OPTIONAL_ROUTINE_STATUS"
+]
 
 from uds.utilities import RID_BIT_LENGTH, RID_MAPPING
 
@@ -11,17 +13,21 @@ from ..data_record import MappingDataRecord, RawDataRecord
 RID = MappingDataRecord(name="RID",
                         length=RID_BIT_LENGTH,
                         values_mapping=RID_MAPPING)
+"""Definition of :ref:`RoutineIdentifier (RID) <knowledge-base-rid>` Data Record."""
 
 ROUTINE_CONTROL_OPTION = RawDataRecord(name="routineControlOption",
                                        length=8,
                                        min_occurrences=0,
                                        max_occurrences=None)
+"""Definition of `routineControlOption` Data Record."""
 
 ROUTINE_STATUS = RawDataRecord(name="routineStatus",
                                length=8,
                                min_occurrences=1,
                                max_occurrences=None)
+"""Definition of `routineStatus` Data Record."""
 OPTIONAL_ROUTINE_STATUS = RawDataRecord(name="routineStatus",
                                         length=8,
                                         min_occurrences=0,
                                         max_occurrences=None)
+"""Definition of optional `routineStatus` Data Record."""
