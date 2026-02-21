e01979b89091b67a980645fa321ff14e89e2fe24
Maciej Dąbrowski <maciej.dabrowski@software-quality.pl>
2026-02-21 20:56:48 +0100

    update examples

--- examples/transport_interface/class_logger.py
+++ examples/transport_interface/class_logger.py
@@ -8,10 +8,10 @@ from uds.transport_interface import AbstractTransportInterface, TransportLogger
 # configure your logging
 # https://docs.python.org/3/library/logging.html
 logger = logging.getLogger("UDS")  # example logger name
-logger.setLevel(logging.INFO)  # example logging level
+logger.setLevel(logging.DEBUG)  # example logging level
 # optionally configure logging to file
 file_handler = logging.FileHandler(filename="uds.log", encoding="utf-8")
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 # optionally configure logging to python console
 stream_handler = logging.StreamHandler()
@@ -22,7 +22,7 @@ logger.addHandler(stream_handler)
 # https://uds.readthedocs.io/en/stable/pages/user_guide/logging.html#configuration
 transport_logger = TransportLogger(logger_name="UDS",  # the same name as previously configured logger
                                    message_logging_level=logging.INFO,
-                                   packet_logging_level=logging.INFO,
+                                   packet_logging_level=logging.DEBUG,
                                    log_sending=True,
                                    log_receiving=True)
 
--- examples/transport_interface/instance_logger.py
+++ examples/transport_interface/instance_logger.py
@@ -11,10 +11,10 @@ transport_interface: AbstractTransportInterface = ...  # TODO: provide your impl
 # configure your logging
 # https://docs.python.org/3/library/logging.html
 logger = logging.getLogger("UDS")  # example logger name
-logger.setLevel(logging.INFO)  # example logging level
+logger.setLevel(logging.DEBUG)  # example logging level
 # optionally configure logging to file
 file_handler = logging.FileHandler(filename="uds.log", encoding="utf-8")
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 # optionally configure logging to python console
 stream_handler = logging.StreamHandler()
@@ -25,7 +25,7 @@ logger.addHandler(stream_handler)
 # https://uds.readthedocs.io/en/stable/pages/user_guide/logging.html#configuration
 transport_logger = TransportLogger(logger_name="UDS",  # the same name as previously configured logger
                                    message_logging_level=logging.INFO,
-                                   packet_logging_level=logging.INFO,
+                                   packet_logging_level=logging.DEBUG,
                                    log_sending=True,
                                    log_receiving=True)
 
